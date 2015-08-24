bd41f1945c6f3e5fb74ce60f3f4a2da1ae5e8a1d
Richard Goater <richard.goater27@gmail.com>
2015-08-24 12:41:36 +0100

    Species definitions added, dynamic species nickname added to URLs

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
 import 'material-design-lite/material.min.css';
 
 import React from 'react';
-import Router, { Route, RouteHandler, Redirect, NotFoundRoute } from 'react-router';
+import Router, { Route, RouteHandler, NotFoundRoute } from 'react-router';
 
-import UploadCollectionPage from './components/collection/UploadCollectionPage.react';
-import Collection from './components/Collection.react';
+import UploadCollection from './components/collection/UploadCollectionPage.react';
+import ExploreCollection from './components/Collection.react';
 import NotFound from './components/NotFound.react';
+
+import speciesData from './species';
 
 class Application extends React.Component {
   render() {
-    return <RouteHandler params={this.props.params} />;
+    return <RouteHandler />;
   }
 }
 
 const routes = (
   <Route name="application" path="/" handler={Application}>
-    <Redirect from="/" to="/collection" />
-    <Route name="collection" path="/collection/?" handler={UploadCollectionPage} />
-    <Route name="collectionExplorer" path="/collection/:id/?" handler={Collection} />
+    <Route name="upload" path=":species/upload/?" handler={UploadCollection} />
+    <Route name="collection" path=":species/collection/:id/?" handler={ExploreCollection} />
     <NotFoundRoute handler={NotFound}/>
   </Route>
 );
 
 Router.run(routes, Router.HistoryLocation, function (Handler, state) {
-  React.render(<Handler params={state.params} />, document.body);
+  const speciesDef = speciesData[state.params.species];
+  if (speciesDef) {
+    React.render(<Handler species={speciesDef}/>, document.body);
+  } else {
+    React.render(<NotFound />, document.body);
+  }
 });
---
+++ src/species.js
@@ -0,0 +1,13 @@
+export default {
+
+  saureus: {
+    id: '1280',
+    name: 'Staphylococcus aureus',
+  },
+
+  salty: {
+    id: '90370',
+    name: 'Salmonella typhimurium',
+  },
+
+};
