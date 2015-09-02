9f63eed77afa089202a82035ac59a42f1b14aa03
Richard Goater <richard.goater27@gmail.com>
2015-09-02 19:59:08 +0100

    Update prod config and home component, rebuild

--- src/components/Home.react.js
+++ src/components/Home.react.js
@@ -5,43 +5,56 @@ import { Link } from 'react-router';
 import Species from '../species';
 import { CGPS } from '../defaults';
 
-const textStyle = {
+const headingStyle = {
   color: CGPS.COLOURS.PURPLE,
+  textAlign: 'center',
 };
 
-const cardStyle = {
-  background: CGPS.COLOURS.PURPLE,
-  textDecoration: 'none',
+const textStyle = {
+  color: CGPS.COLOURS.PURPLE,
+  textTransform: 'none',
+  whiteSpace: 'nowrap',
+  textAlign: 'center',
+  fontSize: '45px',
+  lineHeight: '48px',
+  height: 'calc(48px + 16px)',
+  paddingTop: '8px',
+  marginBottom: '16px',
+  verticalAlign: 'middle',
+};
+
+const listStyle = {
+  margin: 0,
+  padding: 0,
+};
+
+const listItemStyle = {
+  textAlign: 'center',
+  listStyle: 'none',
+  display: 'block',
 };
 
 export default React.createClass({
 
   render: function () {
     return (
-      <div className="mdl-layout">
-        <div className="mdl-grid">
-          <h1 style={textStyle}>WGSA</h1>
-        </div>
-        <div className="mdl-grid">
+      <section>
+        <h1 style={headingStyle}>WGSA</h1>
+        <ul style={listStyle}>
           { Species.list.map((speciesDef) => {
             return (
-              <div className="mdl-cell mdl-cell--6-col">
-                <div className="mdl-card mdl-shadow--2dp">
-                  <div className="mdl-card__title mdl-card--expand" style={cardStyle}></div>
-                  <div className="mdl-card__actions mdl-card--border">
-                    <Link
-                      to={`/${speciesDef.nickname}/upload`}
-                      className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
-                      style={textStyle}>
-                      {speciesDef.name}
-                    </Link>
-                  </div>
-                </div>
-              </div>
+              <li style={listItemStyle}>
+                <Link
+                  to={`/${speciesDef.nickname}/upload`}
+                  className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+                  style={textStyle}>
+                  {speciesDef.formattedName}
+                </Link>
+              </li>
             );
           }) }
-        </div>
-      </div>
+        </ul>
+      </section>
     );
   },
 
