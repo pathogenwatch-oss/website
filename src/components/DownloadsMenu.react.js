63ec80954f9a88beffed98c31fdf1182aa4074fd
Richard Goater <richard.goater27@gmail.com>
2015-09-04 12:19:30 +0100

    Split menu into submenus with headings

--- src/components/DownloadsMenu.react.js
+++ src/components/DownloadsMenu.react.js
@@ -11,29 +11,39 @@ export default React.createClass({
   render() {
     return (
       <ul className={`wgsa-menu mdl-shadow--2dp ${this.props.active ? 'wgsa-menu--is-open' : ''}`}>
-        <li className="wgsa-menu__item">
-          <button className="mdl-button mdl-button--icon">
-            <i className="wgsa-button-icon material-icons">file_download</i>
-          </button>
-          Kernal Checksum Distribution
+        <li>
+          <span className="wgsa-menu-heading">Species Downloads</span>
+          <ul className="wgsa-submenu">
+            <li className="wgsa-menu__item">
+              <button className="mdl-button mdl-button--icon">
+                <i className="wgsa-button-icon material-icons">file_download</i>
+              </button>
+              Population Tree (.nwk)
+            </li>
+          </ul>
         </li>
-        <li className="wgsa-menu__item">
-          <button className="mdl-button mdl-button--icon">
-            <i className="wgsa-button-icon material-icons">file_download</i>
-          </button>
-          Gene Family Alignments
-        </li>
-        <li className="wgsa-menu__item">
-          <button className="mdl-button mdl-button--icon">
-            <i className="wgsa-button-icon material-icons">file_download</i>
-          </button>
-          Collection Tree (.nwk)
-        </li>
-        <li className="wgsa-menu__item">
-          <button className="mdl-button mdl-button--icon">
-            <i className="wgsa-button-icon material-icons">file_download</i>
-          </button>
-          Population Tree (.nwk)
+        <li>
+          <span className="wgsa-menu-heading">Collection Downloads</span>
+          <ul className="wgsa-submenu">
+            <li className="wgsa-menu__item">
+              <button className="mdl-button mdl-button--icon">
+                <i className="wgsa-button-icon material-icons">file_download</i>
+              </button>
+              Kernal Checksum Distribution
+            </li>
+            <li className="wgsa-menu__item">
+              <button className="mdl-button mdl-button--icon">
+                <i className="wgsa-button-icon material-icons">file_download</i>
+              </button>
+              Concatenated Gene Family Alignments
+            </li>
+            <li className="wgsa-menu__item">
+              <button className="mdl-button mdl-button--icon">
+                <i className="wgsa-button-icon material-icons">file_download</i>
+              </button>
+              Collection Tree (.nwk)
+            </li>
+          </ul>
         </li>
       </ul>
     );
