9ade931dcb82ed05a41d36fad9e71a76a9bc8db1
Richard Goater <richard.goater27@gmail.com>
2015-09-12 18:25:33 +0100

    Switch between trees and tables with generic switch component

--- src/components/SpeciesTree.react.js
+++ src/components/SpeciesTree.react.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Tree from './tree/Tree.react';
-import TreeSwitcher from './tree/TreeSwitcher.react';
+import Switch from './Switch.react';
 
 import ReferenceCollectionStore from '../stores/ReferenceCollectionStore';
 import SubtreeStore from '../stores/SubtreeStore';
@@ -88,7 +88,13 @@ export default React.createClass({
     return (
       <Tree
         { ...this.state.tree }
-        navButton={<TreeSwitcher onChange={this.handleTreeSwitch}/>} />
+        navButton={
+          <Switch
+            id="tree-switcher"
+            left={{ title: 'Population Tree', icon: 'nature' }}
+            right={{ title: 'Collection Tree', icon: 'nature_people' }}
+            onChange={this.handleTreeSwitch} />
+        } />
     );
   },
 
