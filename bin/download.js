67c8a50c885fbfe366e12255f998fe1643f4b410
Corin Yeats <dr.yeats@gmail.com>
2015-09-17 22:16:21 +0100

    Improve download script, use macroreact master

--- bin/download.js
+++ bin/download.js
@@ -2,10 +2,21 @@ var fs = require('fs');
 var path = require('path');
 var storageConnection = require('utils/storageConnection');
 
+var documentKey = process.argv[2];
+var outputPath = process.argv[3];
+
 storageConnection.connect(function (error) {
   if (error) throw error;
   var mainStorage = require('services/storage')('main');
-  mainStorage.retrieve(process.argv[2], function (error, result) {
-    console.log(result);
+  mainStorage.retrieve(documentKey, function (error, result) {
+    if (error) {
+      console.err(error);
+      return process.exit(1);
+    }
+    if (outputPath) {
+      process.chdir(outputPath);
+    }
+    fs.writeFileSync(documentKey + '.json', JSON.stringify(result));
+    process.exit(0);
   });
 });
