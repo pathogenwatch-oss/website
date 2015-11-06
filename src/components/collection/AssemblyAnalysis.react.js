c6b5edf512602e08df568b3844e8599710b20051
Richard Goater <richard.goater27@gmail.com>
2015-11-06 11:45:10 +0000

    Change analysis to metrics, send with upload

--- src/components/collection/AssemblyAnalysis.react.js
+++ src/components/collection/AssemblyAnalysis.react.js
@@ -24,12 +24,12 @@ var AssemblyAnalysis = React.createClass({
     var assembly = this.props.assembly;
 
     if (
-      ! assembly.analysis.totalNumberOfNucleotidesInDnaStrings
-      || ! assembly.analysis.totalNumberOfContigs
-      || ! assembly.analysis.smallestNumberOfNucleotidesInDnaStrings
-      || ! assembly.analysis.averageNumberOfNucleotidesInDnaStrings
-      || ! assembly.analysis.biggestNumberOfNucleotidesInDnaStrings
-      || ! assembly.analysis.contigN50
+      ! assembly.metrics.totalNumberOfNucleotidesInDnaStrings
+      || ! assembly.metrics.totalNumberOfContigs
+      || ! assembly.metrics.smallestNumberOfNucleotidesInDnaStrings
+      || ! assembly.metrics.averageNumberOfNucleotidesInDnaStrings
+      || ! assembly.metrics.biggestNumberOfNucleotidesInDnaStrings
+      || ! assembly.metrics.contigN50
     ) {
       return (
         <p className="mdl-card__supporting-text">(Assembly not provided)</p>
@@ -39,31 +39,31 @@ var AssemblyAnalysis = React.createClass({
     return (
       <div className="mdl-grid mdl-grid--no-spacing">
         <div className="mdl-cell mdl-cell--6-col">
-          <AssemblyAnalysisItem label="Assembly Length" value={assembly.analysis.totalNumberOfNucleotidesInDnaStrings} />
+          <AssemblyAnalysisItem label="Assembly Length" value={assembly.metrics.totalNumberOfNucleotidesInDnaStrings} />
         </div>
 
         <div className="mdl-cell mdl-cell--6-col">
-          <AssemblyAnalysisItem label="No. Contigs" value={assembly.analysis.totalNumberOfContigs} />
+          <AssemblyAnalysisItem label="No. Contigs" value={assembly.metrics.totalNumberOfContigs} />
         </div>
 
         <div className="mdl-cell mdl-cell--6-col">
-          <AssemblyAnalysisItem label="Smallest Contig" value={assembly.analysis.smallestNumberOfNucleotidesInDnaStrings} />
+          <AssemblyAnalysisItem label="Smallest Contig" value={assembly.metrics.smallestNumberOfNucleotidesInDnaStrings} />
         </div>
 
         <div className="mdl-cell mdl-cell--6-col">
-          <AssemblyAnalysisItem label="Largest Contig" value={assembly.analysis.biggestNumberOfNucleotidesInDnaStrings} />
+          <AssemblyAnalysisItem label="Largest Contig" value={assembly.metrics.biggestNumberOfNucleotidesInDnaStrings} />
         </div>
 
         <div className="mdl-cell mdl-cell--6-col">
-          <AssemblyAnalysisItem label="Average Contig Length" value={assembly.analysis.averageNumberOfNucleotidesInDnaStrings} />
+          <AssemblyAnalysisItem label="Average Contig Length" value={assembly.metrics.averageNumberOfNucleotidesInDnaStrings} />
         </div>
 
         <div className="mdl-cell mdl-cell--6-col">
-          <AssemblyAnalysisItem label="N50" value={assembly.analysis.contigN50} />
+          <AssemblyAnalysisItem label="N50" value={assembly.metrics.contigN50} />
         </div>
 
         <div className="mdl-cell mdl-cell--6-col">
-          <AssemblyAnalysisItem label="N Count" value={assembly.analysis.totalNumberOfNsInDnaStrings} />
+          <AssemblyAnalysisItem label="N Count" value={assembly.metrics.totalNumberOfNsInDnaStrings} />
         </div>
       </div>
     );
