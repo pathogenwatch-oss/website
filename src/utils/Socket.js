1a2589cfe3e31f162b02df2643eb394476a56478
Richard Goater <richard.goater27@gmail.com>
2016-01-25 12:22:06 +0000

    Transport restriction and test build

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -1,11 +1,16 @@
 import io from 'socket.io-client';
 import CONFIG from '../config';
 
+const options = {
+  transports: [ 'websocket' ],
+};
+
 function socketConnect() {
   if (CONFIG.api) {
+    // no options here to allow dev mode to use long-polling
     return io.connect(CONFIG.api.address);
   }
-  return io.connect();
+  return io.connect(null, options);
 }
 
 module.exports = {
