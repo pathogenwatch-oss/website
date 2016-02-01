5135aacda5cbaa851c4dea05e62cfa9c15c19f83
Richard Goater <richard.goater27@gmail.com>
2016-02-01 19:30:08 +0000

    Sketch of updating progress document

--- microservices/upload-progress.js
+++ microservices/upload-progress.js
@@ -15,15 +15,45 @@ async.parallel({
   }
 
   const { NOTIFICATION } = messageQueueConnection.getExchanges();
-  // const mainStorage = require('services/storage')('main');
+  const mainStorage = require('services/storage')('main');
 
   mqConnection.queue(`upload-progress-queue`, {}, function (queue) {
     LOGGER.info(`${queue.name} is open.`);
     queue.bind(NOTIFICATION.name, '#');
 
     queue.subscribe({ ack: true }, function (message) {
-      LOGGER.debug(message.data.toString());
-      queue.shift();
+      const { taskType, taskStatus, assemblyId, collectionId } =
+        JSON.parse(message.data.toString());
+
+      const documentKey = `UP_${collectionId}`;
+      const assemblyIdString = assemblyId.assemblyId || 'N/A';
+
+      LOGGER.info(`Processing message:
+  Tasktype: ${taskType},
+  Status: ${taskStatus}
+  Assembly Id: ${assemblyIdString}
+  Collection: ${collectionId}`);
+
+      async.waterfall([
+        mainStorage.retrieve.bind(null, documentKey),
+        function (result, done) {
+          const doc = JSON.parse(result);
+
+          if (LOGGER.taskType === 'ERROR') {
+            doc.errors.push({ assemblyId: assemblyIdString, taskType });
+          } else {
+            const numResults = doc.results[taskType] || 0;
+            doc.results[taskType] = numResults + 1;
+          }
+          done(null, doc);
+        },
+        mainStorage.store.bind(null, documentKey)
+      ], function (documentError) {
+        if (documentError) {
+          LOGGER.error(documentError);
+        }
+        queue.shift();
+      });
     });
   });
 });
