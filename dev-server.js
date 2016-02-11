d06bdaf8b7f23bf870b1e017ad124dce8e62e478
Richard Goater <richard.goater27@gmail.com>
2016-02-11 17:54:13 +0000

    Show a toast message if subtree fails to load

--- dev-server.js
+++ dev-server.js
@@ -51,9 +51,12 @@ apiRouter.get('/species/:speciesId/antibiotics', function (req, res) {
   res.sendFile(__dirname + '/static_data/antibiotics.json');
 });
 
+var subtreeError = false;
 apiRouter.get('/species/:speciesId/collection/:collectionId/subtree/:subtreeId', function (req, res) {
+  subtreeError = !subtreeError;
   setTimeout(function () {
-    res.sendFile(`${__dirname}/static_data/${req.params.subtreeId}.json`);
+    return subtreeError ? res.sendStatus(500) :
+      res.sendFile(`${__dirname}/static_data/${req.params.subtreeId}.json`);
   }, 1000);
 });
 
@@ -65,11 +68,11 @@ apiRouter.post('/download/type/assembly/format/fasta', function (req, res) {
   }, 1000);
 });
 
-var error = false;
+var downloadError = false;
 apiRouter.post('/download/type/:idType/format/:fileFormat', function (req, res) {
-  error = !error;
+  downloadError = !downloadError;
   setTimeout(function () {
-    return error ? res.sendStatus(500) :
+    return downloadError ? res.sendStatus(500) :
       res.json({
         'checksum': req.params.fileFormat,
       });
