cc902fae9bab5a2ded73e2411da5f2c4b2995b7d
Richard Goater <richard.goater27@gmail.com>
2016-02-26 00:08:47 +0000

    Add filter information to download menu

--- src/components/explorer/DownloadsMenu.react.js
+++ src/components/explorer/DownloadsMenu.react.js
@@ -15,6 +15,7 @@ const DownloadsMenu = React.createClass({
   propTypes: {
     menuOpen: React.PropTypes.bool,
     files: React.PropTypes.array,
+    counts: React.PropTypes.object,
     closeMenu: React.PropTypes.func,
   },
 
@@ -27,11 +28,20 @@ const DownloadsMenu = React.createClass({
   },
 
   render() {
-    const { menuOpen, files } = this.props;
+    const { menuOpen, files, counts = {} } = this.props;
     return (
       <div className={`wgsa-overlay ${menuOpen ? 'wgsa-overlay--is-visible' : ''}`.trim()}>
-        <div className="wgsa-overlay__content wgsa-downloads-menu mdl-shadow--3dp" onClick={e => e.stopPropagation()}>
+        <div className="wgsa-overlay__content wgsa-downloads-menu mdl-shadow--4dp" onClick={e => e.stopPropagation()}>
           <h3 className="mdl-dialog__title">Downloads</h3>
+          { Object.keys(counts).length ?
+            <p style={{margin: '24px 24px 0'}}>
+              { counts.reference ? <strong>{counts.reference} Reference</strong> : null }
+              { (counts.reference && counts.collection) ? (<span>,&nbsp;</span>) : null }
+              { counts.collection ? (<strong style={{color: '#673c90'}}>{counts.collection} Collection</strong>) : null }
+              { (counts.collection && counts.public) ? (<span>,&nbsp;</span>) : null }
+              { counts.public ? `${counts.public} Public` : null }
+            </p> : null
+          }
           <div className="wgsa-downloads-menu__list">
             <ul className="wgsa-submenu">
               { files.map(fileProps => (
@@ -55,8 +65,9 @@ const DownloadsMenu = React.createClass({
 
 });
 
-function mapStateToProps({ downloads, collection, filter }) {
+function mapStateToProps({ downloads, collection, filter, entities }) {
   return {
+    assemblies: entities.assemblies,
     collectionId: collection.id,
     assemblyIds: [ ...(filter.active ? filter.ids : filter.unfilteredIds) ],
     ...downloads,
@@ -64,9 +75,22 @@ function mapStateToProps({ downloads, collection, filter }) {
 }
 
 function mergeProps(state, { dispatch }) {
-  const { collectionId, assemblyIds, menuOpen, files } = state;
+  const { assemblies, collectionId, assemblyIds, menuOpen, files } = state;
   return {
     menuOpen,
+    counts: assemblyIds.reduce((memo, id) => {
+      const { __isReference, __isCollection } = assemblies[id];
+      if (__isReference) {
+        memo.reference++;
+        return memo;
+      }
+      if (__isCollection) {
+        memo.collection++;
+        return memo;
+      }
+      memo.public++;
+      return memo;
+    }, { reference: 0, collection: 0, public: 0 }),
     files:
       Object.keys(files).
         filter(format => !files[format].notMenu).
