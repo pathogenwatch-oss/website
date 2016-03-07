e7bc218d9c9bb3687ddc6140b72ac0a357a5c7e1
wgsa-admin <cy2@sanger.ac.uk>
2016-03-07 22:29:56 +0000

    Correct ninja scipt

--- bin/ninja.sh
+++ bin/ninja.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 cp ./public/wgsa.js ../wgsa_middle-end/node_modules/wgsa_front-end/public
 cd ../wgsa_middle-end/node_modules/wgsa_front-end
+. ~/.nvm/nvm.sh
 nvm use node
 npm run config
