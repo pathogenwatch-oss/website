912c00a7e0569c6c6057964c76fb1aac974fee84
Richard Goater <richard.goater27@gmail.com>
2016-03-15 10:28:54 +0000

    Add email address to menu bar

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import DownloadsMenu from './components/explorer/DownloadsMenu.react';
 import Staph from './species/saureus';
 
 const MenuLink = ({ isActive, icon, text, link }) => (
-  <Link className={`mdl-navigation__link ${isActive ? 'mdl-navigation__link--active' : null}`.trim()} to={link}>
+  <Link className={`mdl-navigation__link ${isActive ? 'mdl-navigation__link--active' : ''}`.trim()} to={link}>
     <i className="material-icons">{icon}</i>
     {text}
   </Link>
@@ -73,6 +73,7 @@ export default connect(mapStateToProps)(React.createClass({
             ))}
           </nav>
           <img className="cgps-logo" src="/assets/img/CGPS.SHORT.FINAL.svg" />
+          <a className="contact-email" href="mailto:cgps@sanger.ac.uk">cgps@sanger.ac.uk</a>
         </div>
         <main className="mdl-layout__content">
           {children}
