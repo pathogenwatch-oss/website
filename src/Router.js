19d465a763bf7d291915499f77c1cbae64757524
Richard Goater <richard.goater27@gmail.com>
2016-03-15 16:53:57 +0000

    Begin structure for passive view, alllow collections without a collection tree to display

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './App';
 import Home from './components/Home.react';
 import SpeciesHome from './components/SpeciesHome.react';
+import BrowsePublicData from './components/browse';
 import UploadCollection from './components/upload';
 import ExploreCollection from './components/explorer';
 import NotFound from './components/NotFound.react';
@@ -23,6 +24,7 @@ export default () => (
         routes.concat([
           <Route key={nickname} path={nickname} component={SpeciesSetter}>
             <IndexRoute component={SpeciesHome} />
+            <Route path="browse" component={BrowsePublicData} />
             <Route path="upload" component={UploadCollection} />
             <Route path="collection/:id" component={ExploreCollection} />
           </Route>,
