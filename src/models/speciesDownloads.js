1b2914aa6a2de33fb1dbb29c155ca7c3ffbc9501
Richard Goater <richard.goater27@gmail.com>
2016-05-13 19:38:23 +0100

    Add example e2e test for all species downloads

---
+++ src/models/speciesDownloads.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'core_representatives.csv': {
+    contentType: 'text/csv',
+    fileOnDisk: () => 'core_rep_map.tsv',
+  },
+  'reference_fastas.zip': {
+    contentType: 'application/zip',
+    fileOnDisk: () => 'fastas.zip',
+  },
+  'reference_annotations.zip': {
+    contentType: 'application/zip',
+    fileOnDisk: (speciesId) => `wgsa_gff_${speciesId}`,
+  },
+  'reference_metadata.csv': {
+    contentType: 'text/csv',
+    fileOnDisk: () => 'metadata.csv',
+  },
+};
--- src/routes/speciesDownloads.js
+++ src/routes/speciesDownloads.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const downloads = require('models/speciesDownloads');
 const fileModel = require('models/file');
+
 const species = require('wgsa_front-end/universal/species');
 const downloadUtils = require('wgsa_front-end/universal/downloads');
 
@@ -11,25 +13,6 @@ const speciesIds = species.reduce((memo, { nickname, id }) => {
   memo[nickname] = id;
   return memo;
 }, {});
-
-const downloads = {
-  'core_representatives.csv': {
-    contentType: 'text/csv',
-    fileOnDisk: () => 'core_rep_map.tsv',
-  },
-  'reference_fastas.zip': {
-    contentType: 'application/zip',
-    fileOnDisk: () => 'fastas.zip',
-  },
-  'reference_annotations.zip': {
-    contentType: 'application/zip',
-    fileOnDisk: (speciesId) => `wgsa_gff_${speciesId}`,
-  },
-  'reference_metadata.csv': {
-    contentType: 'text/csv',
-    fileOnDisk: () => 'metadata.csv',
-  },
-};
 
 router.get('/species/:nickname/download/:fileName', function (req, res, next) {
   const { nickname, fileName } = req.params;
