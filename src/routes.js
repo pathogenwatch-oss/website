429fa469254f5a3cf35d7806f07091396d576d4f
Richard Goater <richard.goater27@gmail.com>
2016-06-15 21:09:55 +0100

    Add route to redirect to wgsadev zika homepage

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ module.exports = function (app) {
   app.use('/api/', [
     require('routes/collection'),
     require('routes/antibiotic'),
-    require('routes/download')
+    require('routes/download'),
   ]);
+
+  app.use('/zika', (req, res) =>
+    res.redirect('http://wgsadev.pathogensurveillance.net/zikv')
+  );
 };
