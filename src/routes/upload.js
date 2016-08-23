1e1e13b4a534d536f63771a86f8ee811115532b0
Richard Goater <richard.goater27@gmail.com>
2016-08-23 19:24:52 +0100

    Send files to back-end in one request

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -11,10 +11,12 @@ const sketchFilePath = fspath.join(referencesDir, 'refseq-archaea-bacteria-fungi
 const metadataFilePath = fspath.join(referencesDir, 'refseq-archaea-bacteria-fungi-viral-k16-s400.csv');
 const specieator = createMashSpecieator(sketchFilePath, metadataFilePath);
 
+const collectionModel = require('models/collection');
+const assemblyModel = require('models/assembly');
 const analyse = require('wgsa_front-end/universal/fastaAnalysis');
 
 const LOGGER = require('utils/logging').createLogger('Upload');
-const { fastaStoragePath } = require('configuration');
+const { maxCollectionSize = 0, fastaStoragePath } = require('configuration');
 
 router.post('/upload', (req, res) => {
   fastaStorage.store(fastaStoragePath, req.body).
@@ -26,6 +28,44 @@ router.post('/upload', (req, res) => {
       }))
     ).
     then(result => res.json(Object.assign({ metrics: analyse(req.body) }, result)));
+});
+
+router.post('/collection', (req, res, next) => {
+  const { speciesId, files } = req.body;
+
+  LOGGER.info('Received request for new collection id');
+
+  if (!speciesId || !files || !files.length ||
+    (maxCollectionSize && files.length > maxCollectionSize)) {
+    return res.sendStatus(400);
+  }
+
+  collectionModel.add(speciesId, files.map(_ => _.name), (error, result) => {
+    if (error) {
+      return next(error);
+    }
+
+    const { collectionId, assemblyNameToAssemblyIdMap } = result;
+
+    const uploads =
+      files.map(file => {
+        const { storageId, metadata, metrics, name } = file;
+        const assemblyId = assemblyNameToAssemblyIdMap[name];
+        return (
+          fastaStorage.retrieve(fastaStoragePath, storageId).
+            then(sequences =>
+              assemblyModel.beginUpload(
+                { speciesId, collectionId, assemblyId },
+                { sequences, metadata, metrics }
+              )
+            )
+        );
+      });
+
+    Promise.all(uploads).
+      then(() => res.json({ collectionId })).
+      catch(e => next(e));
+  });
 });
 
 module.exports = router;
