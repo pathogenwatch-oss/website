e602a959991a15c17f255cbfe4db900f55aaee64
Richard Goater <richard.goater27@gmail.com>
2016-08-31 19:36:29 +0100

    Tests for thunks created by middleware

--- src/middleware/index.test.js
+++ src/middleware/index.test.js
@@ -2,6 +2,7 @@ import test from 'ava';
 import td from 'testdouble';
 
 import { promiseToThunk } from '.';
+import { createAsyncConstants } from '../actions';
 
 function setup() {
   return { middleware: promiseToThunk(), next: td.function() };
@@ -44,4 +45,52 @@ test('payloads with promises should be converted to thunks',
   }
 );
 
-test.todo('check workings of returned thunk');
+
+function setupThunk(payload) {
+  const { middleware, next } = setup();
+  const type = createAsyncConstants('ACTION');
+  const captor = td.matchers.captor();
+
+  middleware(next)({ type, payload });
+
+  td.verify(next(captor.capture()));
+
+  return { thunk: captor.value, type };
+}
+
+test('converted thunk should fire actions for resolved promises and pass props',
+  async t => {
+    const props = { foo: 'bar' };
+    const result = 'result';
+    const payload = { promise: Promise.resolve(result), ...props };
+    const { type, thunk } = setupThunk(payload);
+    const dispatch = td.function();
+
+    thunk(dispatch);
+
+    await payload.promise;
+
+    td.verify(dispatch({ type: type.ATTEMPT, payload: props }));
+    td.verify(dispatch({ type: type.SUCCESS, payload: { result, ...props } }));
+    t.pass();
+  }
+);
+
+test('converted thunk should fire actions for rejected promises and pass props',
+  async t => {
+    const props = { foo: 'bar' };
+    const payload = { promise: Promise.reject('error'), ...props };
+    const { type, thunk } = setupThunk(payload);
+    const dispatch = td.function();
+
+    thunk(dispatch);
+
+    try {
+      await payload.promise;
+    } catch (error) {
+      td.verify(dispatch({ type: type.ATTEMPT, payload: props }));
+      td.verify(dispatch({ type: type.FAILURE, payload: { error, ...props } }));
+      t.pass();
+    }
+  }
+);
