45b761c873ecc5fa916681d54f71f0d57d9b97f2
Richard Goater <richard.goater27@gmail.com>
2016-09-27 17:12:12 +0100

    Add view-switching buttons, move summary css to own file

--- src/hub/components/Summary.react.js
+++ src/hub/components/Summary.react.js
@@ -1,11 +1,24 @@
+import '../css/summary.css';
+
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 import ProgressBar from '../../components/ProgressBar.react';
 
 import * as selectors from '../selectors';
 
-export const Summary = ({ queueLength, visibleFastas, totalFastas }) => (
+const ViewSwitcher = ({ icon, title, to, pathname }) => (
+  <Link
+    to={to}
+    className={`mdl-button mdl-button--icon wgsa-hub-view-switcher ${to === pathname ? 'wgsa-hub-view-switcher--active' : ''}`.trim()}
+    title={title}
+  >
+    <i className="material-icons">{icon}</i>
+  </Link>
+);
+
+export const Summary = ({ queueLength, visibleFastas, totalFastas, pathname }) => (
   <div className="wgsa-hub-summary">
     { queueLength ?
         <ProgressBar
@@ -15,6 +28,24 @@ export const Summary = ({ queueLength, visibleFastas, totalFastas }) => (
         /> :
         <p>Viewing <span>{visibleFastas}</span> of {totalFastas} assemblies</p>
     }
+    <ViewSwitcher
+      to="/upload"
+      pathname={pathname}
+      title="Grid view"
+      icon="view_module"
+    />
+    <ViewSwitcher
+      to="/upload/map"
+      pathname={pathname}
+      title="Map view"
+      icon="map"
+    />
+    <ViewSwitcher
+      to="/upload/stats"
+      pathname={pathname}
+      title="Stats view"
+      icon="multiline_chart"
+    />
   </div>
 );
 
