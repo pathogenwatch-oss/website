97b9c7d17e75911047d56ace573dc056f813a3f9
Richard Goater <richard.goater27@gmail.com>
2016-10-19 11:33:08 +0100

    style tweaks, rewrite wgsa api links

--- src/documentation-viewer/Markdown.react.js
+++ src/documentation-viewer/Markdown.react.js
@@ -4,12 +4,16 @@ import React from 'react';
 import Markdown from 'react-markdown';
 import { Link } from 'react-router';
 
+import { API_ROOT } from '../utils/Api';
+
 function rewriteMarkdown(markdown) {
   return markdown.replace(
     /\[\[([^\]]+)\]\]/g,
     (a, b) => `[${b}](/documentation/${b.replace(/ /g, '-')})`
   );
 }
+
+const liveApiRoot = 'https://wgsa.net/api';
 
 const renderers = {
   Link: ({ href, title = '', children }) => {
@@ -19,7 +23,7 @@ const renderers = {
       );
     }
     return (
-      <a href={href} title={title}>
+      <a href={href.replace(liveApiRoot, API_ROOT)} title={title}>
         {children}
       </a>
     );
