77e1150927f372e746e1aa68aa7c50845942f3fb
KAD <kad0xf@gmail.com>
2016-10-28 11:40:48 +0100

    Use fasta store v2.0.0

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,19 +1,11 @@
-const fspath = require('path');
-
 const express = require('express');
 const router = express.Router();
 
 const { getCountry } = require('country-reverse-geocoding');
 
 const fastaStorage = require('wgsa-fasta-store');
-const createMashSpecieator = require('mash-specieator');
-const referencesDir = require('mash-sketches');
-const sketchFilePath = fspath.join(referencesDir, 'refseq-archaea-bacteria-fungi-viral-k16-s400.msh');
-const metadataFilePath = fspath.join(referencesDir, 'refseq-archaea-bacteria-fungi-viral-k16-s400.csv');
-const specieator = createMashSpecieator(sketchFilePath, metadataFilePath);
 
 const collectionModel = require('models/collection');
-const analyse = require('wgsa-front-end/universal/fastaAnalysis');
 
 const LOGGER = require('utils/logging').createLogger('Upload');
 const { maxCollectionSize = 0, fastaStoragePath } = require('configuration');
@@ -21,36 +13,33 @@ const { maxCollectionSize = 0, fastaStoragePath } = require('configuration');
 const speciesIds =
   new Set(require('wgsa-front-end/universal/species').map(_ => _.id));
 
-function getWGSASpeciesId(...ids) {
-  for (const id of ids) {
-    if (speciesIds.has(id)) return id;
-  }
+function getWGSASpeciesId(taxId) {
+  if (speciesIds.has(taxId)) return taxId;
   return null;
 }
 
+fastaStorage.setup(fastaStoragePath);
+
 router.post('/upload', (req, res, next) => {
   LOGGER.info('Upload received', req.body.length);
-  fastaStorage.store(fastaStoragePath, req.body).
-    then(({ path, id }) =>
-      specieator.queryFile(path).then(({ speciesTaxId, taxId, scientificName }) => ({
-        speciesId: getWGSASpeciesId(speciesTaxId, taxId),
-        speciesName: scientificName,
-        id,
-      }))
-    ).
-    then(result => {
+
+  fastaStorage.store(fastaStoragePath, req)
+    .then(({ fileId, metrics, specieator: { taxId, scientificName } }) => {
       let country;
 
       if (req.query.lat && req.query.lon) {
-        try {
-          country = getCountry(
-            Number.parseFloat(req.query.lat), Number.parseFloat(req.query.lon)
-          );
-        } catch (e) { return e; }
+        country = getCountry(
+          Number.parseFloat(req.query.lat), Number.parseFloat(req.query.lon)
+        );
       }
 
-      res.json(Object.assign({ country, metrics: analyse(req.body) }, result));
-      req.body = null; // prevent memory leak
+      res.json({
+        id: fileId,
+        speciesId: getWGSASpeciesId(taxId),
+        speciesName: scientificName,
+        metrics,
+        country,
+      });
     }).
     catch(error => next(error));
 });
