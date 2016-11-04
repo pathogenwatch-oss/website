e6f05c9bb0f2ae9d5e6c3a22dd46ed027cc6b85b
KAD <kad0xf@gmail.com>
2016-11-04 10:49:48 +0000

    Revert to default icon

--- src/cgps-commons/LeafletMapCluster.react.js
+++ src/cgps-commons/LeafletMapCluster.react.js
@@ -7,33 +7,19 @@ import 'leaflet.markercluster';
 
 const layerId = {};
 
-const defaultIconProps = {
+const defaultIcon = Leaflet.divIcon({
   className: 'material-icons',
   html: 'place',
   iconSize: [ 40, 40 ],
   iconAnchor: [ 20, 37 ],
   popupAnchor: [ 0, -32 ],
-};
-
-const defaultIcon = Leaflet.divIcon(defaultIconProps);
-
-function getMarkerIcon(iconProps) {
-  return iconProps ?
-    Leaflet.divIcon(Object.assign({}, defaultIconProps, iconProps)) :
-    defaultIcon;
-}
+});
 
 class MarkerCluster extends MapLayer {
 
   componentWillMount() {
     super.componentWillMount();
-    this.leafletElement = Leaflet.markerClusterGroup({
-      maxClusterRadius: 1,
-      iconCreateFunction(cluster) {
-        console.log(cluster.getAllChildMarkers());
-        return Leaflet.divIcon({ html: '<b>' + cluster.getChildCount() + '</b>' });
-      }
-    });
+    this.leafletElement = Leaflet.markerClusterGroup();
   }
 
   componentDidMount() {
@@ -54,8 +40,8 @@ class MarkerCluster extends MapLayer {
       return;
     }
 
-    const layers = markers.map(({ id, position, title, icon }) =>
-      Leaflet.marker(position, { id, icon: getMarkerIcon(icon), title, layerId }).
+    const layers = markers.map(({ id, position, title }) =>
+      Leaflet.marker(position, { id, icon: defaultIcon, title, layerId }).
         on('click', this.props.onMarkerClick)
     );
 
