0366b4c721651785e388915993c5cc76a56b5a13
KAD <kad0xf@gmail.com>
2016-11-04 19:28:45 +0000

    Code clean-up

--- src/collection-viewer/map/selectors.js
+++ src/collection-viewer/map/selectors.js
@@ -6,7 +6,6 @@ export const getBounds = ({ collection: { map: { bounds } } }) => bounds;
 
 export const getLassoPath = ({ collection: { filter: { area } } }) => area;
 
-
 export const getMarkers = createSelector(
   getVisibleAssemblies,
   getColourGetter,
@@ -16,12 +15,8 @@ export const getMarkers = createSelector(
       if (position.latitude && position.longitude) {
         const colour = colourGetter(assembly);
         const key = `${position.latitude},${position.longitude}`;
-        const marker = memo.get(key) || {
-          position,
-          id: [],
-          title: '',
-          slices: new Map(),
-        };
+        const marker = memo.get(key) ||
+          { position, id: [], title: '', slices: new Map() };
         marker.id.push(assemblyId);
         marker.title = marker.id.length;
         marker.slices.set(colour, (marker.slices.get(colour) || 0) + 1);
