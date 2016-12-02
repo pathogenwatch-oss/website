35a98988390992bcbf7cf8338f73f6154938d72b
Richard Goater <richard.goater27@gmail.com>
2016-12-02 17:37:56 +0000

    Use new fasta store, only process files not in the database

--- src/data/fasta.js
+++ src/data/fasta.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const schema = new Schema({
-  id: String,
+  fileId: { type: String, index: true },
   speciesId: String,
   speciesName: String,
   metrics: {
--- src/services/fasta.js
+++ src/services/fasta.js
@@ -1,4 +1,5 @@
 const fastaStorage = require('wgsa-fasta-store');
+const processFasta = require('wgsa-fasta-store/fasta-processor');
 const { fastaStoragePath } = require('configuration');
 fastaStorage.setup(fastaStoragePath);
 
@@ -8,19 +9,31 @@ const { getCountryCode } = require('models/assemblyMetadata');
 const { register } = require('./bus');
 const { ServiceRequestError } = require('../utils/errors');
 
+function getFastaDocument({ fileId, filePath }) {
+  return Fasta.findOne({ fileId }).
+    then(fasta => {
+      if (fasta) return fasta;
+      return processFasta(filePath).then(
+        ({ metrics, specieator: { taxId, scientificName } }) =>
+          Fasta.create({
+            fileId,
+            speciesId: taxId,
+            speciesName: scientificName,
+            metrics,
+          })
+      );
+    });
+}
+
 function store({ stream, metadata }) {
   if (!stream) return Promise.reject(new ServiceRequestError('No stream provided'));
 
   const country = getCountryCode(metadata);
-  return fastaStorage.store(fastaStoragePath, stream)
-    .then(({ fileId, metrics, specieator: { taxId, scientificName } }) =>
-      Fasta.create({
-        id: fileId,
-        speciesId: taxId,
-        speciesName: scientificName,
-        metrics,
-      })
-    ).then(fasta => Object.assign({ country }, fasta.toObject()));
+  return fastaStorage.store(fastaStoragePath, stream).
+    then(getFastaDocument).
+    then(fasta => Object.assign(
+      { country, id: fasta.fileId }, fasta.toObject())
+    );
 }
 
 const role = 'fasta';
