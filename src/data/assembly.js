289e74522f09e2b3c6d74a81dc1742d7f4c11de5
KAD <kad0xf@gmail.com>
2016-12-05 18:13:59 +0000

    Add user assembly mongoose model

---
+++ src/data/assembly.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  _fasta: { type: Schema.Types.ObjectId, ref: 'Fasta' },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
+  name: { type: String, required: true },
+  date: {
+    year: Number,
+    month: Number,
+    day: Number,
+  },
+  position: {
+    latitude: String,
+    longitude: String,
+  },
+  userDefined: Object,
+});
+
+module.exports = mongoose.model('assembly', schema);
