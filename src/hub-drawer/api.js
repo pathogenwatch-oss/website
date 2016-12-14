1921c576b8ecf283667f2ef42ba1313a50b50cbf
Richard Goater <richard.goater27@gmail.com>
2016-12-14 14:28:19 +0000

    Update create collection API

--- src/hub-drawer/api.js
+++ src/hub-drawer/api.js
@@ -1,24 +1,14 @@
 import { API_ROOT } from '../utils/Api';
 
-function formatForSubmit({ id, name, speciesId, metrics, metadata }) {
-  return {
-    id,
-    name,
-    speciesId,
-    metrics,
-    metadata,
-  };
-}
-
-export function createCollectionRequest(files, speciesId, metadata) {
+export function createCollectionRequest(genomes, speciesId, metadata) {
   return $.ajax({
-    type: 'POST',
+    type: 'PUT',
     url: `${API_ROOT}/collection`,
     contentType: 'application/json; charset=UTF-8',
     data: JSON.stringify({
       speciesId,
       ...metadata,
-      files: files.map(formatForSubmit),
+      genomeIds: genomes.map(_ => _.id),
     }),
     dataType: 'json',
   });
