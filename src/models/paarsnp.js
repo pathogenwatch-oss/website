89464954d178c60b532825a801fbe82263f5e072
Richard Goater <richard.goater27@gmail.com>
2017-01-16 17:51:32 +0000

    Sort gene and snp headers naturally

--- src/models/paarsnp.js
+++ src/models/paarsnp.js
@@ -1,3 +1,5 @@
+const naturalSort = require('natural-sort');
+
 const mainStorage = require('services/storage')('main');
 
 const LOGGER = require('utils/logging').createLogger('PAARSNP model');
@@ -7,7 +9,7 @@ function createPaarColumns({ resistanceSets }) {
   return resistanceSets.reduce((memo, { agents, elementIds }) => {
     for (const { antibioticKey } of agents) {
       const elements = (memo[antibioticKey] || []);
-      memo[antibioticKey] = elements.concat(elementIds);
+      memo[antibioticKey] = Array.from(new Set(elements.concat(elementIds))).sort(naturalSort());
     }
     return memo;
   }, {});
@@ -23,7 +25,7 @@ function createSnpColumns({ resistanceSets }) {
           const dividingIndex = element.lastIndexOf('_');
           const gene = element.slice(0, dividingIndex);
           const snp = element.slice(dividingIndex + 1);
-          genes[gene] = (genes[gene] || []).concat(snp);
+          genes[gene] = (genes[gene] || []).concat(snp).sort(naturalSort());
           memo[antibioticKey] = genes;
         }
       }
