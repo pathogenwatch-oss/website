80a3451bfdd345a0b30e160e62f8fd56c33812ab
Richard Goater <richard.goater27@gmail.com>
2017-02-03 17:16:05 +0000

    Revert species Ids to String

--- universal/species.js
+++ universal/species.js
@@ -1,8 +1,8 @@
 module.exports = [
-  { id: 1280, nickname: 'saureus' },
-  { id: 90370, nickname: 'salty' },
-  { id: 485, nickname: 'neigo' },
-  { id: 1646, nickname: 'rensm' },
-  { id: 64320, nickname: 'zikv' },
-  { id: 1313, nickname: 'strpn' },
+  { id: '1280', nickname: 'saureus' },
+  { id: '90370', nickname: 'salty' },
+  { id: '485', nickname: 'neigo' },
+  { id: '1646', nickname: 'rensm' },
+  { id: '64320', nickname: 'zikv' },
+  { id: '1313', nickname: 'strpn' },
 ];
