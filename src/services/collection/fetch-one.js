7487590845f3a9ee0e3f9760c9ca37e6f6b89209
Richard Goater <richard.goater27@gmail.com>
2017-02-08 13:04:10 +0000

    Ensure genomes is an empty array

--- src/services/collection/fetch-one.js
+++ src/services/collection/fetch-one.js
@@ -2,6 +2,12 @@ const services = require('services');
 const CollectionGenome = require('models/collectionGenome');
 
 function addGenomes(collection) {
+  console.log(collection);
+  if (collection.status !== 'READY') {
+    collection.genomes = [];
+    return collection;
+  }
+
   return CollectionGenome.
     find({ _collection: collection.id }, { _collection: 0, fileId: 0 }).
     then(genomes => {
@@ -12,10 +18,8 @@ function addGenomes(collection) {
 
 module.exports = ({ uuid }) =>
   services.request('collection', 'fetch-progress', { uuid }).
-    then(collection => {
-      if (collection.status === 'READY') {
-        return collection.populate('_species').execPopulate().
-          then(_ => addGenomes(_.toObject()));
-      }
-      return collection.toObject();
-    });
+    then(collection => (
+      collection.status === 'READY' ?
+        collection.populate('_species').execPopulate() :
+        collection
+    )).then(_ => addGenomes(_.toObject()));
