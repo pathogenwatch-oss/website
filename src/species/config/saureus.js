2fd4d615bf0b6ec7ca691966a5327c25bdf1c8e6
KAD <kad0xf@gmail.com>
2017-03-01 17:38:30 +0000

    Add description test to saureus

--- src/species/config/saureus.js
+++ src/species/config/saureus.js
@@ -10,6 +10,20 @@ export default {
   formattedName,
   shortName,
   formattedShortName,
+  desc: (
+    <p>
+      <em>Staphylococcus aureus</em> is a Gram-positive microorganism.&nbsp;
+      While the nasal colonisation by <em>Staphylococcus aureus</em> is fairly&nbsp;
+      widespread and occurs asymptomatically,&nbsp;
+      <em>Staphylococcus aureus</em> can also transform into a pathogen causing a diverse&nbsp;
+      infections ranging from mild skin infections to life-threatening conditions.&nbsp;
+      <em>Staphylococcus aureus</em> in its methicillin-sensitive (MSSA) form but&nbsp;
+      especially in its methicillin-resistant (MRSA) or even multi-resistant&nbsp;
+      form are threatening the community and healthcare system worldwide and&nbsp;
+      it has reached the status of being one of the most common causative&nbsp;
+      agents for infectious disease.
+    </p>
+  ),
   maxGenomeSize: 3.5 * Math.pow(10, 6),
   gcRange: {
     min: 31,
