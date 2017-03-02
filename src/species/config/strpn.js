8664ee839e73309dd05f41674bd2acc8c6f6839a
KAD <kad0xf@gmail.com>
2017-03-02 17:26:27 +0000

    Add taxonomy to species config

--- src/species/config/strpn.js
+++ src/species/config/strpn.js
@@ -16,6 +16,19 @@ export default {
       {formattedName}, or pneumococcus, is a Gram-positive, alpha-hemolytic, facultative anaerobic member of the genus <em>Streptococcus</em>. A significant human pathogenic bacterium, {formattedShortName} was recognized as a major cause of pneumonia in the late 19th century, and is the subject of many humoral immunity studies.
     </p>
   ),
+  taxonomy: [
+    { taxId: 131567, scientificName: 'Cellular organisms', rank: 'no rank' },
+    { taxId: 2, scientificName: 'Bacteria', rank: 'superkingdom' },
+    { taxId: 1783272, scientificName: 'Terrabacteria group', rank: 'no rank' },
+    { taxId: 1239, scientificName: 'Firmicutes', rank: 'phylum' },
+    { taxId: 91061, scientificName: 'Bacilli', rank: 'class' },
+    { taxId: 186826, scientificName: 'Lactobacillales', rank: 'order' },
+    { taxId: 1300, scientificName: 'Streptococcaceae', rank: 'family' },
+    { taxId: 1301, scientificName: 'Streptococcus', rank: 'genus' },
+    { taxId: 1313,
+      scientificName: 'Streptococcus pneumoniae',
+      rank: 'species' },
+  ],
   maxGenomeSize: 3 * Math.pow(10, 6),
   publicMetadataColumnNames: [],
   collections: [],
