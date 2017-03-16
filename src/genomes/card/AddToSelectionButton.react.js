f56b3cad1596c9038f60d02e966e1966c8324d16
Richard Goater <richard.goater27@gmail.com>
2017-03-16 20:11:12 +0000

    Remove ripple class from selection button, suspect it is causing console errors

--- src/genomes/card/AddToSelectionButton.react.js
+++ src/genomes/card/AddToSelectionButton.react.js
@@ -23,7 +23,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(
   ({ isSelectable, isSelected, onClick }) => (
    isSelectable ? (
       <button
-        className="mdl-button mdl-button--icon mdl-js-ripple-effect mdl-button--primary"
+        className="mdl-button mdl-button--icon mdl-button--primary"
         onClick={onClick}
         title={ isSelected ? 'Remove from Selection' : 'Add to Selection' }
       >
