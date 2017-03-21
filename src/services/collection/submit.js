345ec625569db6e9085f08c8b6f8ed424b048f90
Richard Goater <richard.goater27@gmail.com>
2017-03-21 13:15:42 +0000

    WIP

---
+++ src/services/collection/submit.js
@@ -0,0 +1,21 @@
+const { request } = require('services/bus');
+
+const Collection = require('models/collection');
+
+module.exports = message => {
+  const { collectionId, collectionGenomes } = message;
+
+  return Collection.findbyId(collectionId)
+    .then(collection =>
+      request('collection', 'add-genomes', { collection, collectionGenomes })
+        .then(() => {
+          request('backend', 'submit', {
+            collectionId: collection.uuid,
+            organismId: collection.organismId,
+            collectionGenomes,
+          });
+          return { slug: collection.slug };
+        })
+        .catch(error => collection.failed(error))
+    );
+};
