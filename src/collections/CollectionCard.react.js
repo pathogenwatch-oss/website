c262e847ce7004d4897ec06c23ec50cd6509be38
KAD <kad0xf@gmail.com>
2017-04-05 15:55:12 +0100

    Add pubmed link to collection card

--- src/collections/CollectionCard.react.js
+++ src/collections/CollectionCard.react.js
@@ -10,35 +10,49 @@ const renderers = {
   Paragraph: (props) => <span>{props.children}</span>,
 };
 
-export default ({ item }) => (
-  <Card className="wgsa-card--bordered">
-    <span className="wgsa-card-metadata-overview">
-      <CardMetadata icon="insert_drive_file">
-        {item.size} genomes
-      </CardMetadata>
-      <CardMetadata title="Created" icon="history">
-        {dateSince(new Date(item.createdAt))} ago
-      </CardMetadata>
-    </span>
-    { item.title ?
-      <Markdown containerTagName="h2" className="wgsa-card-title wgsa-overflow-fade" source={item.title} renderers={renderers} /> :
-      <h2 className="wgsa-card-title">{'(Untitled Collection)'}</h2>
-    }
-    <p className="wgsa-card-subtitle">
-      <FormattedName organismId={item.organismId} fullName />
-    </p>
-    { item.description ?
-      <Markdown source={item.description} /> :
-      <p>(no description)</p>
-    }
-    <div className="wgsa-card-footer">
-      <Link
-        className="mdl-button mdl-button--primary wgsa-button--text"
-        to={`/collection/${item.slug}`}
-      >
-        View Collection
-      </Link>
-      { item.pubmedLink && <a className="mdl-button wgsa-button--text" href={item.pubmedLink} target="_blank" rel="noopener">Pubmed</a>}
-    </div>
-  </Card>
-);
+export default ({ item }) => {
+  const createdAt = new Date(item.createdAt);
+  const { status } = item;
+  return (
+    <Card className="wgsa-card--bordered">
+      <span className="wgsa-card-metadata-overview">
+        <CardMetadata title="Size" icon="insert_drive_file">
+          {item.size} genomes
+        </CardMetadata>
+        <CardMetadata tooltip={createdAt.toLocaleString()} icon="history">
+          {dateSince(createdAt)} ago
+        </CardMetadata>
+      </span>
+      { item.title ?
+        <Markdown containerTagName="h2" className="wgsa-card-title wgsa-overflow-fade" source={item.title} renderers={renderers} /> :
+        <h2 className="wgsa-card-title">{'(Untitled Collection)'}</h2>
+      }
+      <p className="wgsa-card-subtitle">
+        <FormattedName organismId={item.organismId} fullName />
+      </p>
+      { item.description ?
+        <Markdown source={item.description} /> :
+        <p>(no description)</p>
+      }
+      <div className="wgsa-card-footer">
+        <Link
+          className="mdl-button mdl-button--primary wgsa-button--text"
+          to={`/collection/${item.slug}`}
+        >
+          View Collection
+        </Link>
+        { item.pmid &&
+          <a
+            className="mdl-button wgsa-button--text"
+            href={`http://www.ncbi.nlm.nih.gov/pubmed/${item.pmid}`}
+            target="_blank"
+            rel="noopener"
+            title="View Publication"
+          >
+            Pubmed
+          </a>
+        }
+      </div>
+    </Card>
+  );
+};
