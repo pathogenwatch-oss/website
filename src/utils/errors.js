f6bcab8add2dbc9ea66a448b5e95fdd7325fcf0f
KAD <kad0xf@gmail.com>
2017-04-05 16:50:40 +0100

    Add NotFoundError class

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,15 +1,22 @@
 class ServiceRequestError extends Error {
-
   constructor(msg) {
     super(`ServiceRequestError: ${msg}`);
   }
-
   static is(error) {
     return error.message.indexOf('ServiceRequestError') !== -1;
   }
+}
 
+class NotFoundError extends Error {
+  constructor(msg) {
+    super(`NotFoundError: ${msg}`);
+  }
+  static is(error) {
+    return error.message.indexOf('NotFoundError') !== -1;
+  }
 }
 
 module.exports = {
   ServiceRequestError,
+  NotFoundError,
 };
