af924bc0171fd582bf533386739422d762743480
KAD <kad0xf@gmail.com>
2017-04-11 10:31:55 +0100

    Remove console.log messages

--- microservices/runner/manifest.js
+++ microservices/runner/manifest.js
@@ -1,7 +1,5 @@
 const services = require('../../services.json');
 
 module.exports = function getServicesByOrganism(organismId) {
-  console.log(services, organismId);
-  console.log(services.filter(_ => _.organismId === organismId));
   return services.filter(_ => _.organismId === organismId);
 };
