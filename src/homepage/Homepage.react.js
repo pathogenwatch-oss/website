efa0e5da3d8ea89d8d8250230f4eda423baf613b
Richard Goater <richard.goater27@gmail.com>
2017-04-21 13:09:35 +0100

    Add skeleton content to page

--- src/homepage/Homepage.react.js
+++ src/homepage/Homepage.react.js
@@ -3,21 +3,171 @@ import '../css/sonar.css';
 import React from 'react';
 import { Link } from 'react-router';
 
-export default () => (
-  <div className="wgsa-homepage">
-    <section className="jumbotron">
-      <img src="/images/WGSA.FINAL.svg" alt="WGSA" />
-      <h1>Global AMR surveillance through Whole Genome Sequencing</h1>
-    </section>
-    <section className="showcase">
-      <img src="/images/worldmap.svg" />
-      <Link className="showcase__link showcase__link--1 wgsa-sonar-effect" />
-      <Link className="showcase__link showcase__link--2 showcase__link--large wgsa-sonar-effect" />
-      <Link className="showcase__link showcase__link--3 showcase__link--small wgsa-sonar-effect" />
-      <Link className="showcase__link showcase__link--4 wgsa-sonar-effect" />
-      <Link className="showcase__link showcase__link--5 showcase__link--large wgsa-sonar-effect" />
-      <Link className="showcase__link showcase__link--6 wgsa-sonar-effect" />
-      <Link className="showcase__link showcase__link--7 showcase__link--small wgsa-sonar-effect" />
-    </section>
-  </div>
-);
+import { FormattedName } from '../organisms';
+
+export default React.createClass({
+
+  render() {
+    return (
+      <div className="wgsa-homepage">
+        <section className="jumbotron">
+          <img src="/images/WGSA.FINAL.svg" alt="WGSA" />
+          <h1>Global AMR surveillance through Whole Genome Sequencing</h1>
+        </section>
+        <section className="showcase">
+          <img src="/images/worldmap.svg" />
+          <Link className="showcase__link showcase__link--1 wgsa-sonar-effect" />
+          <Link className="showcase__link showcase__link--2 showcase__link--large wgsa-sonar-effect" />
+          <Link className="showcase__link showcase__link--3 showcase__link--small wgsa-sonar-effect" />
+          <Link className="showcase__link showcase__link--4 wgsa-sonar-effect" />
+          <Link className="showcase__link showcase__link--5 showcase__link--large wgsa-sonar-effect" />
+          <Link className="showcase__link showcase__link--6 wgsa-sonar-effect" />
+          <Link className="showcase__link showcase__link--7 showcase__link--small wgsa-sonar-effect" />
+          <footer className="homepage-section-footer">
+            <a href="#about" className="homepage-section-link title-font">
+              <i className="material-icons">expand_more</i> How it works
+            </a>
+          </footer>
+        </section>
+        <section className="how-it-works">
+          <h2>How WGSA Works</h2>
+          <ol>
+            <li>Upload</li>
+            <li>Analyse</li>
+            <li>Explore</li>
+          </ol>
+        </section>
+        <section className="analysis-matrix">
+          <h2>Analysis Matrix</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Organism</th>
+                <th>Core Genome</th>
+                <th>MLST</th>
+                <th>PAARSNP</th>
+                <th>Genotyphi</th>
+                <th>NG-MAST</th>
+                <th>Population Search</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><FormattedName organismId="1280" fullName /></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td></td>
+                <td></td>
+                <td><i className="material-icons">check_circle</i></td>
+              </tr>
+              <tr>
+                <td><FormattedName organismId="90370" fullName /></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td></td>
+                <td><i className="material-icons">check_circle</i></td>
+              </tr>
+              <tr>
+                <td><FormattedName organismId="485" fullName /></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+              </tr>
+              <tr>
+                <td><FormattedName organismId="1313" fullName /></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td></td>
+                <td></td>
+                <td><i className="material-icons">check_circle</i></td>
+              </tr>
+              <tr>
+                <td><FormattedName organismId="64320" fullName /></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td><FormattedName organismId="1646" fullName /></td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <section className="features">
+          <h2>Features</h2>
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Anonymous User</th>
+                <th>Signed-in User</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Access Public Genomes</td>
+                <td><i className="material-icons">check_circle</i></td>
+                <td><i className="material-icons">check_circle</i></td>
+              </tr>
+              <tr>
+                <td>Collection Size</td>
+                <td>150</td>
+                <td>2000</td>
+              </tr>
+              <tr>
+                <td>Private Genomes/Collections</td>
+                <td></td>
+                <td><i className="material-icons">check_circle</i></td>
+              </tr>
+              <tr>
+                <td>Publish Collections</td>
+                <td></td>
+                <td>Coming Soon</td>
+              </tr>
+              <tr>
+                <td>Edit Collection/Genome Metadata</td>
+                <td></td>
+                <td>Coming Soon</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <footer className="cgps-footer mdl-mega-footer">
+          <div className="mdl-mega-footer--top-section">
+            <div className="mdl-mega-footer--left-section">
+              <img className="cgps-supporting-logo" src="images/cgps_logo.png" />
+              <img className="cgps-supporting-logo" src="images/imperial.png" />
+              <img className="cgps-supporting-logo" src="images/wellcome_trust.png" />
+            </div>
+            <div className="mdl-mega-footer--right-section">
+              <a className="cgps-contact-link cgps-contact-link--twitter" target="_blank" href="https://www.twitter.com/TheCGPS">
+                <i className="cgps-contact-link__icon"></i>@<span>TheCGPS</span>
+              </a>
+              <a className="cgps-contact-link cgps-contact-link--email" href="mailto:info@pathogensurveillance.net"><i className="material-icons cgps-contact-link__icon">mail_outline</i><span>info@pathogensurveillance.net</span></a>
+            </div>
+          </div>
+          <div className="mdl-mega-footer--middle-section">
+            <p className="mdl-typography--font-light cgps-copyright">© 2016 Centre for Genomic Pathogen Surveillance</p>
+          </div>
+        </footer>
+      </div>
+    );
+  },
+
+});
