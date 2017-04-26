950cecaa10cb1645c24cdd27f7e65a7217e7228e
Richard Goater <richard.goater27@gmail.com>
2017-04-26 16:43:22 +0100

    Add comment to dev middleware

--- src/dev.js
+++ src/dev.js
@@ -1,6 +1,9 @@
 module.exports = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Credentials', 'true');
-  req.sessionID = 'development';
+
+  // Uncomment when testing upload page, comment out when testing user accounts
+  // req.sessionID = 'development';
+
   next();
 };
