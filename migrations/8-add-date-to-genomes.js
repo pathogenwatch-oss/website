65867432d7d7c05ac3c995910c2ba6bb3bc9af76
Richard Goater <richard.goater27@gmail.com>
2017-04-28 17:29:57 +0100

    Fix genome date migration

--- migrations/8-add-date-to-genomes.js
+++ migrations/8-add-date-to-genomes.js
@@ -1,14 +1,14 @@
 module.exports.id = '8-date-to-genomes';
 
-function convertToDate({ year, month = 1, day = 1 }) {
-  return new Date(year, month - 1, day);
+function convertToDate({ year, month, day }) {
+  return new Date(year, (month || 1) - 1, day || 1);
 }
 
 module.exports.up = function (done) {
   // use this.db for MongoDB communication, and this.log() for logging
   const { db } = this;
   db.collection('genomes')
-    .find({ year: { $exists: true }, date: { $exists: false } })
+    .find({ year: { $exists: true, $ne: null }, date: { $exists: false } })
     .toArray()
     .then(docs =>
       Promise.all(docs.map(doc =>
@@ -24,14 +24,7 @@ module.exports.down = function (done) {
   // use this.db for MongoDB communication, and this.log() for logging
   const { db } = this;
   db.collection('genomes')
-    .find({ date: { $exists: true } })
-    .toArray()
-    .then(docs =>
-      Promise.all(docs.map(doc =>
-        db.collection('genomes')
-          .update({ _id: doc._id }, { $unset: { date: true } })
-      ))
-    )
+    .update({ date: { $exists: true } }, { $unset: { date: true } })
     .then(() => done())
     .catch(done);
 };
