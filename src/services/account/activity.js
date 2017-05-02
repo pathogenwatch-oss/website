6cbb9e3afd3d8412ffdc26c5c2e42b4c6c3a1a38
Richard Goater <richard.goater27@gmail.com>
2017-05-02 21:09:26 +0100

    Add slug and title to collection activity items, ensure combined results are limited

--- src/services/account/activity.js
+++ src/services/account/activity.js
@@ -1,26 +1,32 @@
 const services = require('services');
 const Genome = require('models/genome');
 
+const limit = 10;
+
 function findRecentUploadSessions(user) {
   return Genome.aggregate([
     { $match: { _user: user._id, uploadedAt: { $exists: true } } },
     { $group: { _id: '$uploadedAt', count: { $sum: 1 } } },
     { $project: { _id: 0, count: 1, date: '$_id', type: 'genome' } },
     { $sort: { uploadedAt: -1 } },
-    { $limit: 10 },
+    { $limit: limit },
   ]);
 }
 
 function findRecentCollections(user) {
-  const options = { user, query: { prefilter: 'user', limit: 10 } };
+  const options = { user, query: { prefilter: 'user', limit } };
   return (
     services.request('collection', 'fetch-list', options)
-      .then(collections => collections.map(_ => ({
-        date: _.createdAt,
-        type: 'collection',
-        size: _.size,
-        organismId: _.organismId,
-      })))
+      .then(collections =>
+        collections.map(_ => ({
+          date: _.createdAt,
+          type: 'collection',
+          size: _.size,
+          organismId: _.organismId,
+          slug: _.slug,
+          title: _.title,
+        }))
+      )
   );
 }
 
@@ -30,5 +36,8 @@ module.exports = ({ user }) =>
     findRecentUploadSessions(user),
   ])
   .then(([ collections, uploads ]) =>
-    collections.concat(uploads).sort((a, b) => b.date - a.date)
+    collections
+      .concat(uploads)
+      .sort((a, b) => b.date - a.date)
+      .slice(0, limit) // ensures combined items capped to limit
   );
