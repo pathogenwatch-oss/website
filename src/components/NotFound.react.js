128809c9a59fa64f10d5b056dff76c8bf2ad428a
Richard Goater <richard.goater27@gmail.com>
2017-05-10 15:30:47 +0100

    Tidy "Not found" page

--- src/components/NotFound.react.js
+++ src/components/NotFound.react.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { CGPS } from '../app/constants';
-
-const titleStyle = {
-  textAlign: 'center',
-  color: CGPS.COLOURS.PURPLE,
-};
-
 export default connect()(React.createClass({
+
+  goBack(e) {
+    if (window.history) {
+      e.preventDefault();
+      window.history.back();
+    }
+  },
 
   render() {
     return (
-      <div className="mdl-layout wgsa-loading-container">
+      <div className="mdl-layout wgsa-loading-container wgsa-not-found">
         <div className="wgsa-loading-content">
           <a href="/">
             <img src="/images/WGSA.FINAL.svg" className="wgsa-loading-logo" />
           </a>
-          <h1 style={titleStyle}>
-            These aren't the bacteria you're looking for.
-          </h1>
-          <a style={titleStyle} href="/">Homepage</a>
+          <h1>These aren't the bacteria you're looking for.</h1>
+          <p>(This page could not be found)</p>
+          <a className="mdl-button" onClick={this.goBack} href="#">Go back</a>
         </div>
       </div>
     );
