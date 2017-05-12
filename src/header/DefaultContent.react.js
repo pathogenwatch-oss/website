415c23f28944270360f206c0e109106794c9af08
Richard Goater <richard.goater27@gmail.com>
2017-05-12 10:40:45 +0100

    Enable aside permanently for now

--- src/header/DefaultContent.react.js
+++ src/header/DefaultContent.react.js
@@ -7,7 +7,7 @@ import { isAsideVisible } from './selectors';
 
 import { toggleAside } from './actions';
 
-const DefaultContent = ({ asideVisible, toggle, asideEnabled = false }) => (
+const DefaultContent = ({ asideVisible, toggle, asideEnabled = true }) => (
   <nav className="wgsa-header-content mdl-navigation">
     <NavLink to="/collections/all">Collections</NavLink>
     <NavLink to="/genomes/all">Genomes</NavLink>
