59ad4756789c85a7b65b83bfa6edd41eea474b98
Richard Goater <richard.goater27@gmail.com>
2017-05-15 13:09:53 +0100

    Create RequireLogin component, profile no longer knows about login

--- src/account/index.js
+++ src/account/index.js
@@ -3,10 +3,17 @@ import './styles.css';
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import RequireLogin from './RequireLogin.react';
 import Profile from './Profile.react';
 
 export reducer from './reducer';
 
+const AccountRoute = (props) => (
+  <RequireLogin {...props}>
+    <Profile />
+  </RequireLogin>
+);
+
 export default (
-  <Route path="/account" component={Profile} />
+  <Route path="/account" component={AccountRoute} />
 );
