1a80d5e85ab63b0da807591775b80340f7195744
Richard Goater <richard.goater27@gmail.com>
2017-07-18 11:24:36 +0100

    Use "specieator" as task name and key throughout

--- src/services/genome/process.js
+++ src/services/genome/process.js
@@ -3,15 +3,17 @@ const { request } = require('services/bus');
 const Genome = require('models/genome');
 
 const { tasks } = require('configuration');
-const { taskName, version } = tasks.organism;
+const { version } = tasks.specieator;
+
+const task = 'specieator';
 
 module.exports = function ({ genomeId, fileId, filePath, clientId }) {
-  return request('tasks', 'run', { fileId, task: taskName, version })
+  return request('tasks', 'run', { fileId, task, version })
     .then(result => {
       const organismId = result.speciesTaxId;
       return Promise.all([
         Genome.update({ _id: genomeId }, { organismId }),
-        request('genome', 'add-analysis', { genomeId, task: 'organism', version: `${taskName}-v${version}`, result, clientId }),
+        request('genome', 'add-analysis', { genomeId, task, version, result, clientId }),
         request('tasks', 'submit-genome', { organismId, genomeId, fileId, filePath, clientId }),
       ]);
     });
