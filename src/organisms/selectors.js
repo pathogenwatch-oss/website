8327093c8ae7353ab1584035b9e5d0ad2012b058
Richard Goater <richard.goater27@gmail.com>
2017-08-01 15:57:32 +0100

    Sort organism list on client side

--- src/organisms/selectors.js
+++ src/organisms/selectors.js
@@ -1,3 +1,31 @@
-export const getWgsaOrganisms = ({ organisms }) => organisms.wgsaOrganisms;
+import { createSelector } from 'reselect';
 
-export const getOtherOrganisms = ({ organisms }) => organisms.otherOrganisms;
+import { taxIdMap } from '../organisms';
+
+const getOrganisms = state => state.organisms;
+
+const compareStrings = (a, b) => {
+  if (a > b) return 1;
+  if (a < b) return -1;
+  return 0;
+};
+
+export const getWgsaOrganisms = createSelector(
+  getOrganisms,
+  ({ wgsaOrganisms }) => wgsaOrganisms.sort(
+    (a, b) => compareStrings(
+      taxIdMap.get(a.organismId).name,
+      taxIdMap.get(b.organismId).name
+    )
+  )
+);
+
+export const getOtherOrganisms = createSelector(
+  getOrganisms,
+  ({ otherOrganisms }) => {
+    window.otherOrganisms = otherOrganisms;
+    return otherOrganisms.sort(
+      (a, b) => compareStrings(a.organismName, b.organismName)
+    );
+  }
+);
