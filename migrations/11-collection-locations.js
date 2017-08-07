c6b9ec9267957c7dc5c232c5063ba456370c61ee
KAD <kad0xf@gmail.com>
2017-08-07 21:13:53 +0100

    Save genome locations on collection

---
+++ migrations/11-collection-locations.js
@@ -0,0 +1,38 @@
+module.exports.id = '11-collection-locations';
+
+module.exports.up = function (done) {
+  // use this.db for MongoDB communication, and this.log() for logging
+  const { db } = this;
+
+  db.collection('collections')
+    .find({}, { _id: 1 })
+    .map(({ _id }) =>
+      db.collection('collectiongenomes').aggregate([
+        { $match: { _collection: _id, 'position.latitude': { $exists: true }, 'position.longitude': { $exists: true } } },
+        { $project: { position: 1 } },
+        { $group: { _id: { lat: '$position.latitude', lon: '$position.longitude' } } },
+        { $project: { position: '$_id' } },
+      ])
+      .toArray()
+      .then(docs =>
+        db.collection('collections')
+          .update(
+            { _id },
+            { $set:
+              { locations: docs.map(({ position }) => ([ position.lat, position.lon ])) },
+            }
+          )
+      )
+    )
+    .toArray()
+    .then(() => done())
+    .catch(done);
+};
+
+module.exports.down = function (done) {
+  const { db } = this;
+
+  db.collection('collections').update({}, { $unset: { locations: 1 } }, { multi: true })
+    .then(() => done())
+    .catch(done);
+};
