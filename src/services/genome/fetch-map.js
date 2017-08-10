f7703429314a2734557f9d5b9ed44ce727263c8e
Richard Goater <richard.goater27@gmail.com>
2017-08-10 15:26:30 +0100

    Add organismId to map result

--- src/services/genome/fetch-map.js
+++ src/services/genome/fetch-map.js
@@ -7,14 +7,12 @@ module.exports = function (props) {
   );
   return (
     Genome
-      .find(query, { latitude: 1, longitude: 1, name: 1 })
+      .find(query, { latitude: 1, longitude: 1, name: 1, organismId: 1 })
       .lean()
       .then(docs => {
         for (const doc of docs) {
           doc.id = doc._id;
           doc._id = undefined;
-          doc.title = doc.name;
-          doc.name = undefined;
         }
         return docs;
       })
