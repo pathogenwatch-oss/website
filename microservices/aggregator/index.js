3dd51f863dd5bd4da732f08dbf0f28a9409036a0
Richard Goater <richard.goater27@gmail.com>
2017-08-10 16:56:12 +0100

    Revert aggregator queue names

--- microservices/aggregator/index.js
+++ microservices/aggregator/index.js
@@ -1,7 +1,7 @@
 const handle = require('./handler');
 
 const messageQueueUtil = require('utils/messageQueueConnection');
-const LOGGER = require('utils/logging').createLogger('runner');
+const LOGGER = require('utils/logging').createLogger('aggregator');
 const QUEUE_OPTIONS = { durable: true, autoDelete: false };
 
 module.exports = function ({ mqConnection }) {
@@ -9,9 +9,11 @@ module.exports = function ({ mqConnection }) {
 
   function onMessage(message, _, __, { queue }) {
     // messages from different sources can be different formats
-    const { assemblyId = {} } = (message.data && Buffer.isBuffer(message.data))
-      ? JSON.parse(message.data.toString())
-      : message;
+    if (message.data && Buffer.isBuffer(message.data)) {
+      message = JSON.parse(message.data.toString());
+    }
+
+    const { assemblyId = {} } = message;
     const assemblyIdString = assemblyId.uuid || 'N/A';
 
     LOGGER.info(`Processing message:
@@ -25,12 +27,12 @@ Collection: ${message.collectionId}`);
       then(() => queue.shift()).
       catch(error => {
         LOGGER.error(error);
-        SERVICES.publish('watcher-error', { error, message });
+        SERVICES.publish('aggregator-error', { error, message });
         queue.shift();
       });
   }
 
-  mqConnection.queue('watcher-queue', QUEUE_OPTIONS, queue => {
+  mqConnection.queue('aggregator-queue', QUEUE_OPTIONS, queue => {
     queue.bind(NOTIFICATION.name, '*.*.ASSEMBLY.*');
     queue.bind(NOTIFICATION.name, '*.*.COLLECTION.*');
     queue.bind(NOTIFICATION.name, '*.matrix'); // for reference core results
@@ -38,8 +40,8 @@ Collection: ${message.collectionId}`);
     LOGGER.info(`${queue.name} is open.`);
   });
 
-  mqConnection.queue('watcher-errors-queue', QUEUE_OPTIONS, queue => {
-    queue.bind(SERVICES.name, 'watcher-error');
+  mqConnection.queue('aggregator-errors-queue', QUEUE_OPTIONS, queue => {
+    queue.bind(SERVICES.name, 'aggregator-error');
     LOGGER.info(`${queue.name} is open.`);
   });
 };
