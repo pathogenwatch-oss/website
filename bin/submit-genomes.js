3f4fd322bfd519d2bb0554a4490072ad1b6d604f
Richard Goater <richard.goater27@gmail.com>
2017-08-21 12:38:06 +0100

    Add script to submit existing genomes for speciation.

---
+++ bin/submit-genomes.js
@@ -0,0 +1,36 @@
+const mongoConnection = require('utils/mongoConnection');
+const argv = require('named-argv');
+const mapLimit = require('promise-map-limit');
+
+require('services');
+const Genome = require('models/genome');
+const submit = require('services/genome/specieate');
+
+const limit = 1000;
+
+function parseQuery() {
+  const { query = '{ "analysis.specieator": { "$exists": false } }' } = argv.opts;
+  return JSON.parse(query);
+}
+
+function fetchGenomes(query) {
+  return Genome.find(query, { fileId: 1 }).lean();
+}
+
+function submitGenomes(genomes) {
+  return mapLimit(
+    genomes,
+    limit,
+    ({ _id, fileId }) => submit({ genomeId: _id, fileId })
+  );
+}
+
+mongoConnection.connect()
+  .then(parseQuery)
+  .then(fetchGenomes)
+  .then(submitGenomes)
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
