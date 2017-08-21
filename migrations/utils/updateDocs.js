5e46e8b62286ea1603ed984fe1deb3d2214a767d
Richard Goater <richard.goater27@gmail.com>
2017-08-21 11:28:00 +0100

    Correct typo

--- migrations/utils/updateDocs.js
+++ migrations/utils/updateDocs.js
@@ -1,6 +1,6 @@
 const mapLimit = require('promise-map-limit');
 
-module.exports = function (cursor, updateTteratee, limit = 100) {
+module.exports = function (cursor, updateIteratee, limit = 100) {
   return (
     cursor.toArray()
       .then(docs => {
@@ -8,7 +8,7 @@ module.exports = function (cursor, updateTteratee, limit = 100) {
         let index = 0;
         return mapLimit(docs, limit, doc => {
           process.stdout.write(`\r       Updating document ${++index} of ${docs.length}`);
-          return updateTteratee(doc);
+          return updateIteratee(doc);
         })
         .then(() => console.log(`\n       ${docs.length} documents have been updated`));
       })
