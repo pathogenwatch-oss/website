0cea05440fd653af78945d03d21c793730afb194
Richard Goater <richard.goater27@gmail.com>
2017-08-30 19:55:23 +0100

    Add NG-MAST tab to genome details

--- src/genome-drawer/analysis/index.js
+++ src/genome-drawer/analysis/index.js
@@ -6,10 +6,11 @@ import Metrics from './Metrics.react';
 import MLST from './MLST.react';
 import PAARSNP from './PAARSNP.react';
 import Specieator from './Specieator.react';
+import NgMast from './NgMast.react';
 import renderGenericResults from './Generic.react';
 
 export default (analysis) => {
-  const { mlst, paarsnp, specieator, metrics, ...rest } = analysis;
+  const { metrics, mlst, paarsnp, ngmast, specieator, ...rest } = analysis;
 
   const tabs = [];
 
@@ -17,6 +18,7 @@ export default (analysis) => {
   if (mlst) tabs.push({ key: 'MLST', component: <MLST result={mlst} /> });
   if (paarsnp) tabs.push({ key: 'PAARSNP', component: <PAARSNP {...paarsnp} /> });
   if (specieator) tabs.push({ key: 'Specieator', component: <Specieator result={specieator} /> });
+  if (ngmast) tabs.push({ key: 'NG-MAST', component: <NgMast {...ngmast} /> });
   if (Object.keys(rest).length) tabs.push({ key: 'Other', component: renderGenericResults(rest) });
 
   return tabs;
