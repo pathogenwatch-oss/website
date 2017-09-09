5fd20e472176083709d6113308e143b0897a2893
KAD <kad0xf@gmail.com>
2017-09-09 14:55:05 +0100

    Compare user defined metadata

--- bin/check-metadata-mismatches.js
+++ bin/check-metadata-mismatches.js
@@ -29,7 +29,7 @@ function getMetadata(csvFilePath, delimiter = '', newline = '') {
   const cleanData = data.map(row => {
     const cleanRow = {};
     for (const key of Object.keys(row)) {
-      cleanRow[key.trim()] = row[key].trim();
+      cleanRow[key.trim().toLowerCase()] = row[key];
     }
     return cleanRow;
   });
@@ -57,20 +57,92 @@ function getMetadataAndGenomes({ csv }) {
 function checkGenomeMetadata([ csvRows, genomes ]) {
   // for (const genome of genomes) {
   //   const { _id, name } = genome;
-  //   const metadataRow = csvRows.find(({ DisplayName }) => DisplayName === name);
+  //   const metadataRow = csvRows.find(({ displayname }) => displayname === name);
   //   if (!metadataRow) {
   //     console.error(`Cannot find metadata for genome ${_id}: ${name} in CSV file.`);
   //   }
   //   console.log({ genome, metadataRow });
   // }
-
+  let i = 0;
+  console.log();
   for (let index = 0; index < csvRows.length - 19; index++) {
-    const metadataRow = csvRows[index];
-    // const { DisplayName } = metadataRow;
-    const genome = genomes.find(({ name }) => name.trim() === metadataRow.DisplayName);
+    const csv = csvRows[index];
+    const { displayname } = csv;
+    const genome = genomes.find(({ name }) => name.trim() === displayname.trim());
     if (!genome) {
-      console.error(`Cannot find metadata for genome ${metadataRow.DisplayName} in CSV file.`);
-      console.log({ genome, metadataRow });
+      console.error(`Cannot find metadata for genome ${displayname} in CSV file.`);
+      console.log({ genome, csv });
+      continue;
+    }
+    const wgsaFields = [ 'displayname', 'name', 'filename', 'year', 'month', 'day', 'latitude', 'longitude', 'pmid' ];
+    const userDefined = {};
+    for (const key of Object.keys(csv)) {
+      if (!wgsaFields.includes(key)) {
+        userDefined[key] = csv[key];
+      }
+    }
+    let isMismatch = false;
+    const mismatch = { genome: {}, csv: {} };
+    if ((genome.name || csv.displayname) && genome.name !== csv.displayname) {
+      isMismatch = true;
+      mismatch.genome.name = genome.name;
+      mismatch.csv.name = csv.name;
+    }
+
+    if ((genome.pmid || csv.pmid) && genome.pmid !== csv.pmid) {
+      isMismatch = true;
+      mismatch.genome.pmid = genome.pmid;
+      mismatch.csv.pmid = csv.pmid;
+    }
+
+    if ((genome.latitude || csv.latitude) && genome.latitude !== parseFloat(csv.latitude)) {
+      isMismatch = true;
+      mismatch.genome.latitude = genome.latitude;
+      mismatch.csv.latitude = csv.latitude;
+    }
+
+    if ((genome.longitude || csv.longitude) && genome.longitude !== parseFloat(csv.longitude)) {
+      isMismatch = true;
+      mismatch.genome.longitude = genome.longitude;
+      mismatch.csv.longitude = csv.longitude;
+    }
+
+    if (genome.userDefined || userDefined) {
+      for (const key of Object.keys(genome.userDefined)) {
+        if (genome.userDefined[key] !== userDefined[key]) {
+          isMismatch = true;
+          if (!mismatch.genome.userDefined) {
+            mismatch.genome.userDefined = {};
+          }
+          if (!mismatch.csv.userDefined) {
+            mismatch.csv.userDefined = {};
+          }
+          mismatch.genome.userDefined[key] = genome.userDefined[key];
+          mismatch.csv.userDefined[key] = userDefined[key];
+          break;
+        }
+      }
+      for (const key of Object.keys(userDefined)) {
+        if (genome.userDefined[key] !== userDefined[key]) {
+          isMismatch = true;
+          if (!mismatch.genome.userDefined) {
+            mismatch.genome.userDefined = {};
+          }
+          if (!mismatch.csv.userDefined) {
+            mismatch.csv.userDefined = {};
+          }
+          mismatch.genome.userDefined[key] = genome.userDefined[key];
+          mismatch.csv.userDefined[key] = userDefined[key];
+          break;
+        }
+      }
+    }
+
+    if (isMismatch) {
+      console.log('Mismatch %s', ++i, genome._id, genome.name);
+      console.log(mismatch);
+      console.log();
+      // console.log({ genome, csv });
     }
   }
 
