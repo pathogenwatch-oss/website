0957cbe554808bb4cb9ac4ce61bc9cecd9a139b2
Richard Goater <richard.goater27@gmail.com>
2017-09-14 19:20:12 +0100

    Use new PAARSNP and MLST structures, implement antibiotic selecting for mechanisms.

--- src/genome-drawer/analysis/PAARSNP.react.js
+++ src/genome-drawer/analysis/PAARSNP.react.js
@@ -1,39 +1,79 @@
 import React from 'react';
-
+import classnames from 'classnames';
 import { Section, Metadata } from '../components';
 
-export default ({ __v, antibiotics = {}, snp =[], paar = [] }) => (
-  <Section heading="PAARSNP" version={__v}>
-    <dl>
-      <Metadata large label="Antibiotics">
-        <table>
-          <thead>
-            <tr>
-              { Object.keys(antibiotics).map(key => <th key={key} title={antibiotics[key].fullName}>{key}</th>) }
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              { Object.keys(antibiotics).map(key => {
-                const { state, fullName } = antibiotics[key];
-                return (
-                  <td key={key}>
-                    { state !== 'UNKNOWN' && state !== 'NOT_FOUND' &&
-                      <i
-                        title={`${fullName}: ${state}`}
-                        className={`material-icons wgsa-amr--${state.toLowerCase()}`}
-                      >
-                        lens
-                      </i> }
-                  </td>
-                );
-              }) }
-            </tr>
-          </tbody>
-        </table>
-      </Metadata>
-      <Metadata label="SNPs">{snp.join(', ')}</Metadata>
-      <Metadata label="Genes">{paar.join(', ')}</Metadata>
-    </dl>
-  </Section>
-);
+export default React.createClass({
+
+  getInitialState() {
+    return {
+      antibiotic: null,
+    };
+  },
+
+  getTableHeaders() {
+    const { antibiotics = [] } = this.props;
+    const { antibiotic } = this.state;
+    return antibiotics.map(({ name, fullName }) =>
+      <th
+        key={name}
+        title={fullName}
+        className={classnames({ active: antibiotic.name === name })}
+      >
+        {name}
+      </th>
+    );
+  },
+
+  getTableCells() {
+    const { antibiotics = [] } = this.props;
+    return antibiotics.map(antibiotic => {
+      const { name, fullName, state } = antibiotic;
+      return (
+        <td className="wgsa-amr" key={name}>
+          { state !== 'UNKNOWN' && state !== 'NOT_FOUND' &&
+            <i
+              title={`${fullName}: ${state}`}
+              className={`material-icons wgsa-amr--${state.toLowerCase()}`}
+              onClick={() => this.setState({ antibiotic })}
+            >
+              lens
+            </i> }
+        </td>
+      );
+    });
+  },
+
+  render() {
+    const { __v, snp = [], paar = [] } = this.props;
+    const { antibiotic } = this.state;
+    return (
+      <Section heading="PAARSNP" version={__v}>
+        <dl>
+          <Metadata large label="Antibiotics">
+            <table>
+              <thead>
+                <tr>
+                  { this.getTableHeaders() }
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  { this.getTableCells() }
+                </tr>
+              </tbody>
+            </table>
+          </Metadata>
+          { antibiotic &&
+            <Metadata label="SNPs">
+              {antibiotic.mechanisms.filter(m => snp.includes(m)).join(', ')}
+            </Metadata> }
+          { antibiotic &&
+            <Metadata label="Genes">
+              {antibiotic.mechanisms.filter(m => paar.includes(m)).join(', ')}
+            </Metadata> }
+        </dl>
+      </Section>
+    );
+  },
+
+});
