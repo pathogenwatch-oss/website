ba03b0b0b5e992cfd96da1a2c9c4cdec1971c49f
Richard Goater <richard.goater27@gmail.com>
2017-09-14 19:19:38 +0100

    WIP

--- src/services/genome/add-analysis.js
+++ src/services/genome/add-analysis.js
@@ -3,18 +3,23 @@ const { request } = require('services/bus');
 const Genome = require('models/genome');
 
 const formatters = {
+  mlst: ({ st, url, genes, alleles }) => ({
+    st,
+    url,
+    alleles: genes.map(gene => ({ gene, hits: alleles[gene].map(_ => _.id) })),
+  }),
   paarsnp: result => ({
     antibiotics: result.resistanceProfile ?
-      result.resistanceProfile.reduce(
-        (memo, { agent, resistanceState, resistanceSets }) => {
-          memo[agent.name] = {
-            fullName: agent.fullName,
-            state: resistanceState,
-            mechanisms: resistanceSets.map(_ => _.resistanceSetName),
-          };
-          return memo;
-        }, {}
-      ) : {},
+      result.resistanceProfile.map(
+        ({ agent, resistanceState, resistanceSets }) => ({
+          name: agent.name,
+          fullName: agent.fullName,
+          state: resistanceState,
+          mechanisms: resistanceSets.reduce(
+            (memo, _) => memo.concat(_.elementIds), []
+          ),
+        })
+      ) : [],
     paar: result.paarResult ?
       result.paarResult.paarElementIds || [] : [],
     snp: result.snparResult ?
