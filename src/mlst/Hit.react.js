0051e9cd40a5ad016181e1d2dc881d5b8ef25a5a
Richard Goater <richard.goater27@gmail.com>
2017-09-25 11:43:16 +0100

    Remove css import, fix react keys

--- src/mlst/Hit.react.js
+++ src/mlst/Hit.react.js
@@ -1,5 +1,3 @@
-import './styles.css';
-
 import React from 'react';
 
 import { isNovel } from './utils';
--- src/mlst/Profile.react.js
+++ src/mlst/Profile.react.js
@@ -4,9 +4,9 @@ import Hit from './Hit.react';
 
 export default ({ alleles = [] }) => (
   <span className="wgsa-mlst-profile">
-    {alleles.map(({ hits }) =>
-      <span className="wgsa-mlst-profile-section">
-        {hits.map(id => <Hit id={id} textOnly />)}
+    {alleles.map(({ gene, hits }) =>
+      <span key={gene} className="wgsa-mlst-profile-section">
+        {hits.map(id => <Hit key={id} id={id} textOnly />)}
       </span>
     )}
   </span>
