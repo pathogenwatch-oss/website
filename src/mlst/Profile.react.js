3d0e505b2f384bc028385d7b7f3e6f851f34f576
Richard Goater <richard.goater27@gmail.com>
2017-10-13 22:19:22 +0100

    Render missing MLST alleles in the collection viewer and typing download

--- src/mlst/Profile.react.js
+++ src/mlst/Profile.react.js
@@ -4,9 +4,11 @@ import Hit from './Hit.react';
 
 export default ({ alleles = [] }) => (
   <span className="wgsa-mlst-profile">
-    {alleles.map(({ gene, hits }) =>
+    { alleles.map(({ gene, hits }) =>
       <span key={gene} className="wgsa-mlst-profile-section">
-        {hits.map(id => <Hit key={id} id={id} textOnly />)}
+        { hits.length ?
+          hits.map(id => <Hit key={id} id={id} textOnly />) :
+          '?' }
       </span>
     )}
   </span>
