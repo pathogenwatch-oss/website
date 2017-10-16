f49bf9476802337e82ca0788e8e8f1d52454d8d9
Richard Goater <richard.goater27@gmail.com>
2017-10-16 16:48:45 +0100

    Add title and cursor to missing alleles in profile

--- src/mlst/Profile.react.js
+++ src/mlst/Profile.react.js
@@ -8,7 +8,7 @@ export default ({ alleles = [] }) => (
       <span key={gene} className="wgsa-mlst-profile-section">
         { hits.length ?
           hits.map(id => <Hit key={id} id={id} textOnly />) :
-          '?' }
+          <span title="Not Found" className="wgsa-mlst-hit">?</span> }
       </span>
     )}
   </span>
