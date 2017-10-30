333060a01f98cc2b83f4b00c90f687c6422ed0b0
Richard Goater <richard.goater27@gmail.com>
2017-10-30 15:53:20 +0000

    Revert "Do not pull genome analysis when creating collection"
    This reverts commit e6b4a79b88c2e523589a0600efc15c1c4422c71a.

--- src/services/collection/create.js
+++ src/services/collection/create.js
@@ -47,7 +47,7 @@ function createCollection({ organismId, genomeIds, title, description, pmid, use
 }
 
 function getGenomes(ids) {
-  return Genome.find({ _id: { $in: ids } }, { analysis: 0 });
+  return Genome.find({ _id: { $in: ids } });
 }
 
 function getCollectionAndGenomes(message) {
