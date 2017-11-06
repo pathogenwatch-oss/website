8bc65d1c4ee84e462ce12cd60bcf0b9f9e8bab93
Richard Goater <richard.goater27@gmail.com>
2017-11-06 11:54:15 +0000

    Update typhi picture, add stop typhoid link

--- src/organisms/config/salty.js
+++ src/organisms/config/salty.js
@@ -27,6 +27,9 @@ export default {
       <p>
         <strong><em>Salmonella enterica</em> subsp. <em>enterica</em> serovar Typhi</strong> causes typhoid (enteric) fever, a disease that affects approximately 20-30 million of new patients every year,<sup>[1,2]</sup> predominantly in Asia and sub-Saharan Africa. Mortality rates in patients who are not administered antibiotic treatment can be as high as 26%.<sup>[1]</sup> The emergence of multi-drug resistant Salmonella Typhi and the shortage of local surveillance data from endemic regions both challenge the management of typhoid fever.
       </p>
+      <p>
+        For more information go to <a href="http://www.stoptyphoid.org" target="_blank" rel="noopener">www.stoptyphoid.org</a>.
+      </p>
       <ol className="wgsa-species-citations">
         <li><cite>Wain J., Hendriksen R.S., Mikoleit M.L., Keddy K.H. and Ochiai R.L. 2015. Typhoid Fever. Lancet 385:1136-1145.</cite></li>
         <li><cite>Crump J.A. and Mintz E.D. 2010. Global trends in typhoid and paratyphoid fever. Clinical Infectious Diseases 50:241-246.</cite></li>
