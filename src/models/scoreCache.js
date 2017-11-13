347e6640c5012fcb3616729382013f8edd3c3473
KAD <kad0xf@gmail.com>
2017-11-13 17:24:02 +0000

    Cache scores in mongodb

---
+++ src/models/scoreCache.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  fileId: String,
+  version: String,
+  scores: Object,
+});
+
+schema.index({ fileId: 1, version: 1 });
+
+module.exports = mongoose.model('ScoreCache', schema);
--- src/services/tasks/run-collection.js
+++ src/services/tasks/run-collection.js
@@ -3,58 +3,73 @@
 
 const docker = require('docker-run');
 const crypto = require('crypto');
+const es = require('event-stream');
 
 const Analysis = require('models/analysis');
 const CollectionGenome = require('../../models/collectionGenome');
+const ScoreCache = require('../../models/scoreCache');
 
 const { getImageName } = require('manifest.js');
 
 const LOGGER = require('utils/logging').createLogger('runner');
 
 function runTask(task, version, collectionId, requires, organismId) {
-  return new Promise((resolve, reject) => {
-    const container = docker(getImageName(task, version), {
-      env: {
-        WGSA_ORGANISM_TAXID: organismId,
-        WGSA_COLLECTION_ID: collectionId,
-      },
-      remove: false,
-    });
-    const stream = CollectionGenome.collection.find(
-      { _collection: collectionId },
-      requires.reduce((projection, requiredTask) => {
-        projection[`analysis.${requiredTask}`] = 1;
-        return projection;
-      }, {}),
-      { raw: true }
-    );
-    stream.pipe(container.stdin);
-    const buffer = [];
-    container.stdout.on('data', (data) => {
-      buffer.push(data.toString());
-    });
-    container.on('exit', (exitCode) => {
-      LOGGER.info('exit', exitCode);
-      if (exitCode !== 0) {
-        container.stderr.setEncoding('utf8');
-        reject(new Error(container.stderr.read()));
-      } else if (buffer.length === 0) {
-        reject(new Error('No output received.'));
-      } else {
-        let output;
-        try {
-          output = JSON.parse(buffer.join(''));
-        } catch (e) {
-          reject(e);
+  return CollectionGenome.find({ _collection: collectionId }, { fileId: 1 }).lean()
+    .then(fileIds => new Promise((resolve, reject) => {
+      const fileIdByGenomeId = fileIds.reduce((memo, doc) => {
+        memo[doc._id] = doc.fileId;
+        return memo;
+      }, {});
+      const container = docker(getImageName(task, version), {
+        env: {
+          WGSA_ORGANISM_TAXID: organismId,
+          WGSA_COLLECTION_ID: collectionId,
+        },
+        remove: false,
+      });
+      const stream = CollectionGenome.collection.find(
+        { _collection: collectionId },
+        requires.reduce((projection, requiredTask) => {
+          projection[`analysis.${requiredTask}`] = 1;
+          return projection;
+        }, { fileId: 1 }),
+        { raw: true }
+      );
+      stream.pipe(container.stdin);
+
+      container.stdout
+        .pipe(es.split())
+        .on('data', (data) => {
+          try {
+            const doc = JSON.parse(data);
+            if (doc.id && doc.scores) {
+              const update = {};
+              for (const key of Object.keys(doc.scores)) {
+                update[`scores.${fileIdByGenomeId[key]}`] = doc.scores[key];
+              }
+              console.log({ fileId: fileIdByGenomeId[doc.id], version }, update);
+              ScoreCache.update({ fileId: fileIdByGenomeId[doc.id], version }, update, { upsert: true })
+                .then(updated => console.log({ updated }));
+            } else {
+              resolve(doc);
+            }
+          } catch (e) {
+            reject(e);
+          }
+        });
+
+      container.on('exit', (exitCode) => {
+        LOGGER.info('exit', exitCode);
+        if (exitCode !== 0) {
+          container.stderr.setEncoding('utf8');
+          reject(new Error(container.stderr.read()));
         }
-        resolve(output);
-      }
-    });
-    container.on('spawn', (containerId) => {
-      LOGGER.info('spawn', containerId, 'running task', task, 'for collection', collectionId);
-    });
-    container.on('error', reject);
-  });
+      });
+      container.on('spawn', (containerId) => {
+        LOGGER.info('spawn', containerId, 'running task', task, 'for collection', collectionId);
+      });
+      container.on('error', reject);
+    }));
 }
 
 module.exports = function handleMessage({ collectionId, task, version, requires, organismId }) {
