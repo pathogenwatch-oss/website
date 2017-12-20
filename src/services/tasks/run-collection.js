4e816492bd1ecde8d2c406c3c32798d55ba70fbf
KAD <kad0xf@gmail.com>
2017-12-20 17:39:24 +0000

    Send filter flags to tree containers

--- src/services/tasks/run-collection.js
+++ src/services/tasks/run-collection.js
@@ -90,11 +90,14 @@ function attachInputStream(container, spec, genomes, uncachedFileIds) {
     {
       _id: { $in: genomes.map(_ => _._id) },
       fileId: { $in: uncachedFileIds },
-      'analysis.core.profile.filter': false,
-      'analysis.core.profile.alleles.filter': false,
+      // 'analysis.core.profile.filter': false,
+      // 'analysis.core.profile.alleles.filter': false,
     },
     {
+      name: 1,
       fileId: 1,
+      'analysis.core.profile.filter': 1,
+      'analysis.core.profile.alleles.filter': 1,
       'analysis.core.profile.id': 1,
       'analysis.core.profile.alleles.id': 1,
       'analysis.core.profile.alleles.rstart': 1,
