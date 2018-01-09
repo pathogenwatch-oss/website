c999a137c156de3f9993b989868dd2083b532436
KAD <kad0xf@gmail.com>
2018-01-09 21:53:48 +0000

    Report some random numbers as stats

--- src/routes/download/collection/variance-summary.js
+++ src/routes/download/collection/variance-summary.js
@@ -14,6 +14,8 @@ function getGenomes(genomeIds) {
   const projection = {
     fileId: 1,
     'analysis.core.profile.filter': 1,
+    'analysis.core.profile.alleles.filter': 1,
+    'analysis.core.profile.alleles.mutations': 1,
   };
   const options = {
     sort: { fileId: 1 },
@@ -42,13 +44,19 @@ function getCache(genomes, { version }) {
   });
 }
 
-async function generateTreeData(tree, genomeIds) {
+async function generateTreeData(tree, collectionGenomeIds) {
+  const genomeIds = Collection.getSubtreeIds(tree);
   const genomes = await getGenomes(genomeIds);
   const cache = await getCache(genomes, tree);
   const scores = [];
+  let userVariantSites = 0;
+  let publicVariantSites = 0;
+  let userUnfilteredVariantSites = 0;
+  let publicUnfilteredVariantSites = 0;
+  const uniqueUserVariantSites = {};
+  const uniquePublicVariantSites = {};
   for (let a = 0; a < genomes.length; a++) {
     const genomeA = genomes[a];
-    // const isPopulation = !collectionGenomes.has(id);
     for (let b = 0; b <= a; b++) {
       const genomeB = genomes[b];
       if (genomeA === genomeB) {
@@ -59,8 +67,42 @@ async function generateTreeData(tree, genomeIds) {
         throw new ServiceRequestError(`Missing score for ${genomeA.fileId} ${genomeB.fileId}`);
       }
     }
+
+    const isCollectionGenome = collectionGenomeIds.has(genomeA._id.toString());
+    for (const profile of genomeA.analysis.core.profile) {
+      if (!uniqueUserVariantSites[profile.id]) {
+        uniqueUserVariantSites[profile.id] = new Set();
+      }
+      if (!uniquePublicVariantSites[profile.id]) {
+        uniquePublicVariantSites[profile.id] = new Set();
+      }
+      for (const allele of profile.alleles) {
+        for (const site of Object.keys(allele.mutations)) {
+          if (profile.filter === false && allele.filter === false) {
+            if (isCollectionGenome) {
+              userVariantSites++;
+              uniqueUserVariantSites[profile.id].add(site);
+            }
+            publicVariantSites++;
+            uniquePublicVariantSites[profile.id].add(site);
+          }
+          if (isCollectionGenome) {
+            userUnfilteredVariantSites++;
+          }
+          publicUnfilteredVariantSites++;
+        }
+      }
+    }
   }
   const stats = calculateStats(scores.sort());
+  let userRepresentativeVariantSites = 0;
+  for (const id of Object.keys(uniqueUserVariantSites)) {
+    userRepresentativeVariantSites += uniqueUserVariantSites[id].size;
+  }
+  let publicRepresentativeVariantSites = 0;
+  for (const id of Object.keys(uniquePublicVariantSites)) {
+    publicRepresentativeVariantSites += uniquePublicVariantSites[id].size;
+  }
   const result = {
     label: tree.name,
     totalCollection: tree.size - tree.populationSize,
@@ -72,6 +114,12 @@ async function generateTreeData(tree, genomeIds) {
     medianScore: stats.median,
     lowerQuartileScore: stats.lowerQuartile,
     upperQuartileScore: stats.upperQuartile,
+    userVariantSites,
+    publicVariantSites,
+    userUnfilteredVariantSites,
+    publicUnfilteredVariantSites,
+    userRepresentativeVariantSites,
+    publicRepresentativeVariantSites,
   };
   return result;
 }
@@ -111,12 +159,12 @@ function writeMatrixLine(result, stream) {
     result.medianScore,
     result.lowerQuartileScore,
     result.upperQuartileScore,
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
+    result.userVariantSites,
+    result.publicVariantSites,
+    result.userUnfilteredVariantSites,
+    result.publicUnfilteredVariantSites,
+    result.userRepresentativeVariantSites,
+    result.publicRepresentativeVariantSites,
   ];
   stream.write(line.join(','));
   stream.write('\n');
@@ -129,13 +177,12 @@ function writeMatrixFooter(stream) {
 }
 
 async function generateData({ genomes, tree, subtrees }, stream) {
-  // const collectionGenomes = genomes.map(id => id.toString());
+  const collectionGenomeIds = new Set(genomes.map(id => id.toString()));
   // const collectionData = await generateTreeData('collection', collectionGenomes.length, 0, collectionGenomes);
   // writeMatrixLine(collectionData, stream);
 
   for (const subtree of subtrees) {
-    const genomeIds = Collection.getSubtreeIds(subtree);
-    const data = await generateTreeData(subtree, genomeIds);
+    const data = await generateTreeData(subtree, collectionGenomeIds);
     writeMatrixLine(data, stream);
   }
 }
