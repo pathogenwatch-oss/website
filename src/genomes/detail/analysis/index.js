02f99ffab8a2599e189df47de162fa10ca9df5f6
Richard Goater <richard.goater27@gmail.com>
2018-01-24 10:20:33 +0000

    Add Core tab to genome detail

--- src/genomes/detail/analysis/index.js
+++ src/genomes/detail/analysis/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 import { VersionSwitcher } from '../components';
 
 import Metrics from './Metrics.react';
+import Core from './Core.react';
 import MLST from './MLST.react';
 import PAARSNP from './PAARSNP.react';
 import Speciator from './Speciator.react';
@@ -14,17 +15,58 @@ import renderGenericResults from './Generic.react';
 
 export default (genome) => {
   const { analysis = {} } = genome;
-  const { metrics, mlst, paarsnp, genotyphi, ngmast, speciator, ...rest } = analysis;
+  const { metrics, core, mlst, paarsnp, genotyphi, ngmast, speciator, ...rest } = analysis;
 
   const tabs = [];
 
-  if (metrics) tabs.push({ key: 'Metrics', component: <VersionSwitcher taskName="metrics" component={Metrics} genome={genome} /> });
-  if (mlst) tabs.push({ key: 'MLST', component: <VersionSwitcher taskName="mlst" component={MLST} genome={genome} /> });
-  if (paarsnp) tabs.push({ key: 'AMR', component: <VersionSwitcher taskName="paarsnp" component={PAARSNP} genome={genome} /> });
-  if (genotyphi) tabs.push({ key: 'Genotyphi', component: <VersionSwitcher taskName="genotyphi" component={Genotyphi} genome={genome} /> });
-  if (ngmast) tabs.push({ key: 'NG-MAST', component: <VersionSwitcher taskName="ngmast" component={NgMast} genome={genome} /> });
-  if (speciator) tabs.push({ key: 'Organism', component: <VersionSwitcher taskName="speciator" component={Speciator} genome={genome} /> });
-  if (Object.keys(rest).length) tabs.push({ key: 'Other', component: renderGenericResults(rest) });
+  if (metrics) {
+    tabs.push({
+      key: 'Metrics',
+      component: <VersionSwitcher taskName="metrics" component={Metrics} genome={genome} />,
+    });
+  }
+  if (core) {
+    tabs.push({
+      key: 'Core',
+      component: <VersionSwitcher taskName="core" component={Core} genome={genome} />,
+    });
+  }
+  if (mlst) {
+    tabs.push({
+      key: 'MLST',
+      component: <VersionSwitcher taskName="mlst" component={MLST} genome={genome} />,
+    });
+  }
+  if (paarsnp) {
+    tabs.push({
+      key: 'AMR',
+      component: <VersionSwitcher taskName="paarsnp" component={PAARSNP} genome={genome} />,
+    });
+  }
+  if (genotyphi) {
+    tabs.push({
+      key: 'Genotyphi',
+      component: <VersionSwitcher taskName="genotyphi" component={Genotyphi} genome={genome} />,
+    });
+  }
+  if (ngmast) {
+    tabs.push({
+      key: 'NG-MAST',
+      component: <VersionSwitcher taskName="ngmast" component={NgMast} genome={genome} />,
+    });
+  }
+  if (speciator) {
+    tabs.push({
+      key: 'Organism',
+      component: <VersionSwitcher taskName="speciator" component={Speciator} genome={genome} />,
+    });
+  }
+  if (Object.keys(rest).length) {
+    tabs.push({
+      key: 'Other',
+      component: renderGenericResults(rest),
+    });
+  }
 
   return tabs;
 };
