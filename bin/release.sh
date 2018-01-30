d73da9ece51264b3c58465b9ae0862806ca76b84
Richard Goater <richard.goater27@gmail.com>
2018-01-30 19:37:49 +0000

    Ensure all new files are committed on release

--- bin/release.sh
+++ bin/release.sh
@@ -6,7 +6,7 @@ LEVEL=${1:-"minor"}
 echo Creating new $LEVEL release...
 
 npm run build
-git add public
+git add public views monitor.json
 git commit -m "Build"
 npm version $LEVEL
 git push && git push --tags
