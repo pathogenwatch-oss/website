0aa0ac8296beabe2348d4ab291f6b24683f1c613
Richard Goater <richard.goater27@gmail.com>
2018-01-31 18:25:21 +0000

    Move cleanup to release script

--- bin/release.sh
+++ bin/release.sh
@@ -5,8 +5,13 @@ LEVEL=${1:-"minor"}
 
 echo Creating new $LEVEL release...
 
+rm -f public/{wgsa,vendor,manifest}.*.js
+rm -f public/{compress,csv}-worker.*.js
+
 npm run build
+
 git add public views monitor.json
 git commit -m "Build"
+
 npm version $LEVEL
 git push && git push --tags
