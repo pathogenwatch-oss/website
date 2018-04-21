6f7b1b1a3863d920d84f9625f2e153b1fe2bae8a
Richard Goater <richard.goater27@gmail.com>
2018-04-21 12:10:18 +0100

    Send tree name with tree failure message

--- src/services/collection/add-error.js
+++ src/services/collection/add-error.js
@@ -3,17 +3,17 @@ const Collection = require('../../models/collection');
 
 module.exports = async function ({ spec, metadata }) {
   const { task } = spec;
-  const { collectionId, clientId } = metadata;
+  const { collectionId, clientId, name } = metadata;
 
   if (task === 'tree') {
     await Collection.update({ _id: collectionId }, { 'tree.status': 'FAILED' });
   } else {
     await Collection.update(
-      { _id: collectionId, 'subtrees.name': metadata.name },
+      { _id: collectionId, 'subtrees.name': name },
       { 'subtrees.$.status': 'FAILED' }
     );
   }
 
-  const payload = { task, status: 'FAILED' };
+  const payload = { task, name, status: 'FAILED' };
   return request('collection', 'send-progress', { clientId, payload });
 };
