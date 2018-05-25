2d29854781ca8318ad512e8ef9288fe35655434b
Richard Goater <richard.goater27@gmail.com>
2018-05-25 19:36:57 +0100

    Add button to re-run clustering

--- src/genomes/report/analysis/Clustering.react.js
+++ src/genomes/report/analysis/Clustering.react.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 import Spinner from '../../../components/Spinner.react';
 import Notify from '../../../components/Notify.react';
@@ -16,6 +17,17 @@ const Clustering = React.createClass({
     }
   },
 
+  renderButton(label = 'Cluster Now', primary = true) {
+    return (
+      <button
+        className={classnames('mdl-button mdl-button--raised', { 'mdl-button--colored': primary })}
+        onClick={this.props.onClick}
+      >
+        {label}
+      </button>
+    );
+  },
+
   render() {
     return (
       <React.Fragment>
@@ -23,7 +35,12 @@ const Clustering = React.createClass({
         {(() => {
           switch (this.props.status) {
             case 'ERROR':
-              return <p>Something went wrong :(</p>;
+              return (
+                <React.Fragment>
+                  <p>Something went wrong :(</p>
+                  {this.renderButton('Try Again', false)}
+                </React.Fragment>
+              );
             case 'LOADING':
             case 'PENDING':
               return (
@@ -56,18 +73,19 @@ const Clustering = React.createClass({
                 if (others < 1) return <li key={t}>No cluster found at threshold { t }</li>;
                 return <li key={t}><Link to={`/clustering/${genomeId}?threshold=${t}`}>{ others } other genome{ others !== 1 ? 's' : ''}</Link> at threshold { t }</li>;
               });
-              return <React.Fragment><p>This genome is found in clusters with:</p><ul>{ bullets }</ul></React.Fragment>;
+              return (
+                <React.Fragment>
+                  <p>This genome is found in clusters with:</p>
+                  <ul>{bullets}</ul>
+                  {this.renderButton('Run Again', false)}
+                </React.Fragment>
+              );
             }
             default:
               return (
                 <React.Fragment>
                   <p>Clusters have not yet been determined for this genome.</p>
-                  <button
-                    className="mdl-button mdl-button--raised mdl-button--colored"
-                    onClick={this.props.onClick}
-                  >
-                    Cluster Now
-                  </button>
+                  {this.renderButton()}
                 </React.Fragment>
               );
           }
