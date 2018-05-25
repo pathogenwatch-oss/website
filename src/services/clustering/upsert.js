b94912f62866c31cbc5b875c95ccf745804930a4
Richard Goater <richard.goater27@gmail.com>
2018-05-25 18:52:30 +0100

    Fix adding clusters whe signed in

--- src/services/clustering/upsert.js
+++ src/services/clustering/upsert.js
@@ -5,10 +5,16 @@ module.exports = async function ({ metadata, results }) {
   const { user, sessionID, scheme, clientId } = metadata;
 
   const query = { scheme };
-  if (user) query.user = user;
-  else query.sessionID = sessionID;
+  const update = { scheme, results };
+  if (user) {
+    query.user = user._id;
+    update.user = user._id;
+  } else {
+    query.sessionID = sessionID;
+    update.sessionID = sessionID;
+  }
 
-  await Clustering.update(query, { user, sessionID, scheme, results }, { upsert: true });
+  await Clustering.update(query, update, { upsert: true });
 
   return request('clustering', 'send-progress', { clientId, payload: { status: 'READY' } });
 };
