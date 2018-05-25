f7082377ea774a0f8f015c5f3fc73ff473ca59e9
Richard Goater <richard.goater27@gmail.com>
2018-05-25 11:43:22 +0100

    Fix issue fetching clusters when signed in

--- src/services/clustering/fetch.js
+++ src/services/clustering/fetch.js
@@ -1,7 +1,13 @@
 const Clustering = require('../../models/clustering');
 
 module.exports = async function ({ user, sessionID, genomeId, scheme }) {
-  const clusters = await Clustering.findOne({ $or: [ { user }, { sessionID } ], scheme });
+  const query = { scheme };
+  if (user) {
+    query.user = user._id;
+  } else if (sessionID) {
+    query.sessionID = sessionID;
+  }
+  const clusters = await Clustering.findOne(query);
   if (!clusters) return {};
   const { results } = clusters;
 
