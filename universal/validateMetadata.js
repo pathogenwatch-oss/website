08f31f0514538ea499cfa9e865b51934c77cd9b3
Richard Goater <richard.goater27@gmail.com>
2018-07-13 11:41:30 +0100

    Improve typechecking

--- universal/validateMetadata.js
+++ universal/validateMetadata.js
@@ -11,21 +11,41 @@ module.exports = function validateMetadata(row) {
 
   let error;
 
-  if (name && name.length > 256) {
+  if (name && typeof name !== 'string') {
+    error = 'name is wrong type';
+  } else if (name && name.length > 256) {
     error = 'name is too long';
+  } else if (pmid && typeof pmid !== 'string') {
+    error = 'pmid is wrong type';
   } else if (pmid && pmid.length > 16) {
     error = 'pmid is too long';
+  } else if (typeof latitude === 'object' || isNaN(latitude)) {
+    error = 'latitude is not a number';
+  } else if (typeof longitude === 'object' || isNaN(longitude)) {
+    error = 'longitude is not a number';
+  } else if (typeof year === 'object' || isNaN(year)) {
+    error = 'year is not a number';
+  } else if (typeof month === 'object' || isNaN(month)) {
+    error = 'month is not a number';
+  } else if (typeof day === 'object' || isNaN(day)) {
+    error = 'day is not a number';
   } else if (Object.keys(userDefined).length > 64) {
     error = 'more than 64 user-defined columns';
-  } else if (
-    Object.entries(userDefined)
-      .some(([ key, value ]) => key.length > 256 || value.length > 256)
-  ) {
-    error = 'user-defined key or value too long';
-  } else if (isNaN(latitude) || isNaN(longitude)) {
-    error = 'latitude or longitude is not a number';
-  } else if (isNaN(year) || isNaN(month) || isNaN(day)) {
-    error = 'year/month/day is not a number';
+  } else {
+    for (const [ key, value ] of Object.entries(userDefined)) {
+      if (typeof value !== 'string') {
+        error = 'user-defined value is not a string';
+        break;
+      }
+      if (value.length > 256) {
+        error = 'user-defined value is too long';
+        break;
+      }
+      if (key.length > 256) {
+        error = 'user-defined column heading is too long';
+        break;
+      }
+    }
   }
 
   if (error) {
