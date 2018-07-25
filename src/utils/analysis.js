7dc055f6318e4bf75c6d8b3a35474d080f947ec8
KAD <kad0xf@gmail.com>
2018-07-25 08:44:02 +0100

    Move subspecies mapping from speciator task to middle-end

--- src/utils/analysis.js
+++ src/utils/analysis.js
@@ -1,5 +1,25 @@
+const { getSubspecies } = require('./organisms');
+
 function summariseAnalysis(analysis) {
   const { task, version, results } = analysis;
+
+  if (task === 'speciator') {
+    const subspecies = getSubspecies(results.taxId);
+    if (subspecies !== null) {
+      return {
+        __v: version,
+        organismId: subspecies.taxId,
+        organismName: subspecies.name,
+        ...results,
+      };
+    }
+    return {
+      __v: version,
+      organismId: results.speciesId,
+      organismName: results.speciesName,
+      ...results,
+    };
+  }
 
   // core and cgmlst documents are big so don't store them all on the genome record
   if (task === 'core') {
