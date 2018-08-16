a7ba4e346b1d5e117cd0f2cf3b597f5ed52507b7
Richard Goater <richard.goater27@gmail.com>
2018-08-16 19:29:37 +0100

    Throw error if queue position requested for unqueued task

--- src/services/clustering/fetch-position.js
+++ src/services/clustering/fetch-position.js
@@ -1,13 +1,19 @@
 const { request } = require('services');
 const { queues } = require('../taskQueue');
 
+const { NotFoundError } = require('../../utils/errors');
+
 module.exports = async function ({ user, genomeId, date }) {
   let until = date;
   if (!until) {
     const projection = { dateCreated: 1 };
     const { doc } =
       await request('clustering', 'fetch-queue-message', { user, genomeId, projection });
-    until = doc.dateCreated;
+    if (doc) {
+      until = doc.dateCreated;
+    } else {
+      throw new NotFoundError('Clustering job not on queue');
+    }
   }
   const { position } =
      await request('tasks', 'queue-position', { until, type: queues.clustering });
