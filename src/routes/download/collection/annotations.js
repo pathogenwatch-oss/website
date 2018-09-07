9326303fe5fb75569ea19c7cb47af377408961af
Richard Goater <richard.goater27@gmail.com>
2018-09-07 15:31:58 +0100

    Fix GFF download

--- src/routes/download/collection/annotations.js
+++ src/routes/download/collection/annotations.js
@@ -1,38 +1,96 @@
 const transform = require('stream-transform');
 const ZipStream = require('zip-stream');
-const async = require('async');
 const sanitize = require('sanitize-filename');
+const es = require('event-stream');
+const { Writable } = require('stream');
 
 const Genome = require('models/genome');
+const Analysis = require('models/analysis');
 const { request } = require('services');
 const LOGGER = require('utils/logging').createLogger('Downloads');
 
 const header = '##gff-version 3.2.1';
 
-function getCollectionGenomes({ genomes }, genomeIds) {
+function getGenomeSummaries(query) {
+  const projection = {
+    name: 1,
+    fileId: 1,
+    'analysis.core.__v': 1,
+    'analysis.mlst.matches': 1,
+    'analysis.paarsnp': 1,
+  };
+  const options = { sort: { name: 1 } };
+  const genomes = Genome.find(query, projection, options).lean().cursor();
+  const fileIds = new Set();
+  const genomeLookup = {};
+  const coreVersionMap = {};
+  return new Promise((resolve, reject) => {
+    genomes.on('data', genome => {
+      const { fileId, analysis: { core } } = genome;
+      genomeLookup[fileId] = genomeLookup[fileId] || [];
+      genomeLookup[fileId].push(genome);
+      coreVersionMap[core.__v] = coreVersionMap[core.__v] || [];
+      coreVersionMap[core.__v].push(fileId);
+      fileIds.add(fileId);
+    });
+    genomes.on('error', err => reject(err));
+    genomes.on('end', () => resolve({
+      fileIds: [ ...fileIds ],
+      genomeLookup,
+      coreVersionMap,
+    }));
+  });
+}
+
+function getGenomes(fileIds, genomeLookup, coreVersionMap) {
+  const query = {
+    task: 'core',
+    $or: [],
+  };
+
+  for (const version of Object.keys(coreVersionMap)) {
+    query.$or.push({ version, fileId: { $in: coreVersionMap[version] } });
+    coreVersionMap[version] = null;
+  }
+
+  const cores = Analysis.find(query, {
+    fileId: 1,
+    version: 1,
+    results: 1,
+  }).lean().cursor();
+
+  const coreFormatter = es.through(function (core) {
+    const { fileId, version, results } = core;
+    const genomes = genomeLookup[fileId] || [];
+    for (const genome of genomes) {
+      if (!genome.analysis || !genome.analysis.core) continue;
+      if (genome.analysis.core.__v === version) {
+        this.emit('data', {
+          ...genome,
+          analysis: {
+            ...genome.analysis,
+            core: results,
+          },
+        });
+      }
+    }
+    genomeLookup[fileId] = null;
+  });
+
+  return cores.pipe(coreFormatter);
+}
+
+async function getCollectionGenomes({ genomes }, genomeIds) {
   const query = {
     _id: { $in: genomeIds },
     $or: [
       { _id: { $in: genomes } },
       { public: true },
     ],
-    'analysis.core': { $exists: true },
-    'analysis.mlst': { $exists: true },
-    'analysis.paarsnp': { $exists: true },
   };
-  const projection = {
-    name: 1,
-    'analysis.core.profile': 1,
-    'analysis.mlst.matches': 1,
-    'analysis.paarsnp': 1,
-  };
-  const options = {
-    sort: {
-      name: 1,
-    },
-  };
-  return Genome
-    .find(query, projection, options);
+  const summaries = await getGenomeSummaries(query);
+  const { fileIds, genomeLookup, coreVersionMap } = summaries;
+  return getGenomes(fileIds, genomeLookup, coreVersionMap);
 }
 
 function gffTransformer(input) {
@@ -60,178 +118,183 @@ function convertDocumentToGFF(doc, stream) {
 
   stream.write(header);
 
-  // const profile = core.profile;
-  const profile = core.profile.map(x => x.familyId).sort().map(x => core.profile.find(y => y.familyId === x));
-  for (const { familyId, refLength, alleles } of profile) {
-    for (const allele of alleles) {
+  if (core) {
+    const profile = core.profile.map(x => x.id).sort().map(x => core.profile.find(y => y.id === x));
+    for (const { id, rlength, alleles } of profile) {
+      for (const allele of alleles) {
+        stream.write({
+          sequence: allele.qid,
+          source: 'Pathogenwatch_Core',
+          type: 'CDS',
+          start: allele.qstart,
+          end: allele.qstop,
+          score: allele.pid,
+          reversed: (allele.rstart > allele.rstop),
+          phase: (allele.rstart - 1) % 3,
+          attributes: {
+            ID: `CORE_${allele.qid}_${allele.rstart}_${allele.rstop}`,
+            name: id,
+            target: `${id} ${allele.rstart} ${rlength}`,
+            targetLength: rlength,
+            notes: [
+              `Paralogue ${alleles.length}`,
+              allele.complete ? 'Complete Match' : 'Partial Match',
+            ].join(','),
+            evalue: allele.evalue,
+          },
+        });
+      }
+    }
+  }
+
+  if (mlst) {
+    for (const { gene, id, start, end, contig } of mlst.matches) {
       stream.write({
-        sequence: allele.qId,
-        source: 'WGSA_Core',
-        type: 'CDS',
-        start: allele.qR[0],
-        end: allele.qR[1],
-        score: allele.pid,
-        reversed: (allele.qR[0] > allele.qR[1]),
-        phase: (allele.rR[0] - 1) % 3,
+        sequence: contig,
+        source: 'Pathogenwatch_MLST',
+        type: 'genetic_marker',
+        start,
+        end,
+        score: null, // match.identity,
+        reversed: end < start,
+        phase: null,
         attributes: {
-          ID: `CORE_${allele.qId}_${allele.qR[0]}_${allele.qR[1]}`,
-          name: familyId,
-          target: `${familyId} ${allele.rR[0]} ${refLength}`,
-          targetLength: refLength,
-          notes: [
-            `Paralogue ${alleles.length}`,
-            allele.full ? 'Complete Match' : 'Partial Match',
-          ].join(','),
-          evalue: allele.evalue,
+          ID: `MLST_${contig}_${start}_${end}`,
+          name: `${gene}_${id}`,
+          // target: `${match.reference.id} ${match.reference.start} ${match.reference.length}`,
+          // targetLength: match.reference.length,
+          note: `FamilyAllele ${id}`,
+          // evalue: match.evalue,
         },
       });
     }
   }
 
-  for (const { gene, id, start, end, contig } of mlst.matches) {
-    stream.write({
-      sequence: contig,
-      source: 'WGSA_MLST',
-      type: 'genetic_marker',
-      start,
-      end,
-      score: null, // match.identity,
-      reversed: end < start,
-      phase: null,
-      attributes: {
-        ID: `MLST_${contig}_${start}_${end}`,
-        name: `${gene}_${id}`,
-        // target: `${match.reference.id} ${match.reference.start} ${match.reference.length}`,
-        // targetLength: match.reference.length,
-        note: `FamilyAllele ${id}`,
-        // evalue: match.evalue,
-      },
-    });
-  }
-
-  for (const match of paarsnp.matches) {
-    if (match.source === 'WGSA_PAAR') {
-      stream.write({
-        sequence: match.query.id,
-        source: match.source,
-        type: match.type,
-        start: match.query.start,
-        end: match.query.stop,
-        score: match.identity,
-        reversed: match.reversed,
-        phase: (match.query.start - 1) % 3,
-        attributes: {
-          ID: `PAAR_${match.query.id}_${match.query.start}_${match.query.stop}`,
-          name: match.id,
-          target: `${match.id} ${match.library.start} ${match.library.length}`,
-          note: match.agents.join(','),
-          evalue: match.evalue,
-          targetLength: match.library.length,
-        },
-      });
-    } else if (match.type === 'point_mutation') {
-      stream.write({
-        sequence: match.id,
-        source: match.source,
-        type: match.type,
-        start: match.queryLocation,
-        end: match.queryLocation,
-        score: null,
-        reversed: match.reversed,
-        phase: null,
-        attributes: {
-          ID: `SNPAR_${match.name}`,
-          name: match.name,
-          parent: `${match.id}_${match.libraryStart}`,
-          target: `${match.id} ${match.referenceLocation} ${match.referenceLocation}`,
-          note: match.agents.join(','),
-        },
-      });
-    } else {
-      stream.write({
-        sequence: match.query.id,
-        source: match.source,
-        type: match.type,
-        start: match.query.start,
-        end: match.query.stop,
-        score: match.identity,
-        reversed: match.reversed,
-        phase: (match.query.start - 1) % 3,
-        attributes: {
-          ID: `SNPAR_${match.query.id}_${match.query.start}_${match.query.stop}`,
-          name: match.id,
-          target: `${match.library.id} ${match.library.start} ${match.library.length}`,
-          evalue: match.evalue,
-          targetLength: match.library.length,
-        },
-      });
+  if (paarsnp) {
+    for (const match of paarsnp.matches) {
+      if (match.source.indexOf('PAAR') !== -1) {
+        stream.write({
+          sequence: match.query.id,
+          source: 'Pathogenwatch_PAAR',
+          type: match.type,
+          start: match.query.start,
+          end: match.query.stop,
+          score: match.identity,
+          reversed: match.reversed,
+          phase: (match.query.start - 1) % 3,
+          attributes: {
+            ID: `PAAR_${match.query.id}_${match.query.start}_${match.query.stop}`,
+            name: match.id,
+            target: `${match.id} ${match.library.start} ${match.library.length}`,
+            targetLength: match.library.length,
+            note: match.agents.join(','),
+            evalue: match.evalue,
+          },
+        });
+      } else if (match.type === 'point_mutation') {
+        stream.write({
+          sequence: match.id,
+          source: 'Pathogenwatch_SNPAR',
+          type: match.type,
+          start: match.queryLocation,
+          end: match.queryLocation,
+          score: null,
+          reversed: match.reversed,
+          phase: null,
+          attributes: {
+            ID: `SNPAR_${match.name}`,
+            name: match.name,
+            parent: `${match.id}_${match.libraryStart}`,
+            target: `${match.id} ${match.referenceLocation} ${match.referenceLocation}`,
+            note: match.agents.join(','),
+          },
+        });
+      } else {
+        stream.write({
+          sequence: match.query.id,
+          source: 'Pathogenwatch_SNPAR',
+          type: match.type,
+          start: match.query.start,
+          end: match.query.stop,
+          score: match.identity,
+          reversed: match.reversed,
+          phase: (match.query.start - 1) % 3,
+          attributes: {
+            ID: `SNPAR_${match.query.id}_${match.query.start}_${match.query.stop}`,
+            name: match.id,
+            target: `${match.library.id} ${match.library.start} ${match.library.length}`,
+            targetLength: match.library.length,
+            evalue: match.evalue,
+          },
+        });
+      }
     }
   }
 }
 
-function generateData(collection, genomeIds, filename, res, next) {
-  const cursor = getCollectionGenomes(collection, genomeIds);
+async function generateData(collection, genomeIds, filename, res, next) {
+  const genomes = await getCollectionGenomes(collection, genomeIds);
+  genomes.on('error', next);
+
   if (genomeIds && genomeIds.length === 1) {
-    cursor.then(([ doc ]) => {
-      const stream = transform(gffTransformer);
+    const stream = transform(gffTransformer);
+    stream.on('error', next);
+    stream.pipe(res);
+
+    genomes.on('data', doc => {
       res.setHeader('Content-Disposition', `attachment; filename=${filename || `${doc.name}.gff`}`);
       res.setHeader('Content-Type', 'text/plain');
-      stream.pipe(res);
       convertDocumentToGFF(doc, stream);
-      stream.end();
     });
+    genomes.on('end', () => stream.end());
   } else {
-    res.setHeader('Content-Disposition', `attachment; filename=${filename || 'wgsa-annotations.zip'}`);
+    res.setHeader('Content-Disposition', `attachment; filename=${filename || 'pathogenwatch-annotations.zip'}`);
     res.setHeader('Content-Type', 'application/zip');
 
     const archive = new ZipStream();
     archive.on('error', next);
     archive.pipe(res);
 
-    const _cursor = cursor.lean().cursor();
-
-    async.doUntil(done => {
-      _cursor.next((error, doc) => {
-        if (error) {
-          done(error);
-          return;
-        }
+    const createArchive = new Writable({
+      objectMode: true,
+      write(doc, _, callback) {
         if (!doc) {
-          done(null, true);
+          callback();
           return;
         }
         const stream = transform(gffTransformer);
         archive.entry(stream, { name: `${doc.name}.gff` }, (err) => {
           if (err) {
-            done(err);
+            callback(err);
             return;
           }
-          done(null, false);
+          callback();
         });
         convertDocumentToGFF(doc, stream);
         stream.end();
-      });
-    },
-    isDone => isDone,
-    (error) => {
-      if (error) {
-        next(error);
-        return;
-      }
-      archive.finish();
+      },
+      final(callback) {
+        archive.finish();
+        callback();
+      },
     });
+
+    genomes.pipe(createArchive);
   }
 }
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const { user } = req;
   const { token } = req.params;
   const { ids } = req.method === 'GET' ? req.query : req.body;
   const { filename: rawFilename = '' } = req.query;
-  const filename = sanitize(rawFilename) || 'annotations.csv';
+  const filename = sanitize(rawFilename);
   const genomeIds = ids ? ids.split(',') : null;
 
-  request('collection', 'authorise', { user, token, projection: { genomes: 1 } })
-    .then(collection => generateData(collection, genomeIds, filename, res, next))
-    .catch(next);
+  const collection = await request('collection', 'authorise', { user, token, projection: { genomes: 1 } });
+  try {
+    await generateData(collection, genomeIds, filename, res, next);
+  } catch (e) {
+    next(e);
+  }
 };
