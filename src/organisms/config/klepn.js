d0bd04d5907463fb845a167583ebcbf207343c5e
Corin Yeats <corin.yeats@gmail.com>
2018-09-17 14:22:17 +0100

    Created K. pneumoniae species record card.

---
+++ src/organisms/config/klepn.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const name = 'Klebsiella pneumoniae sensu stricto';
+const formattedName = (<em>{name}</em>);
+
+const shortName = 'K. pneumoniae';
+const formattedShortName = (<em>{shortName}</em>);
+
+export default {
+  name,
+  formattedName,
+  shortName,
+  formattedShortName,
+  maxGenomeSize: 4 * Math.pow(10, 6),
+  desc: (
+    <p>
+      <strong>{formattedName}</strong> https://en.wikipedia.org/wiki/Klebsiella_pneumoniae
+    </p>
+  ),
+  taxonomy: [
+    { taxId: 131567, scientificName: 'Cellular organisms', rank: 'no rank' },
+    { taxId: 2, scientificName: 'Bacteria', rank: 'superkingdom' },
+    { taxId: 1224, scientificName: 'Proteobacteria', rank: 'phylum' },
+    { taxId: 1236, scientificName: 'Gammaproteobacteria', rank: 'class' },
+    { taxId: 91347, scientificName: 'Enterobacteriales', rank: 'order' },
+    { taxId: 543, scientificName: 'Enterobacteriaceae', rank: 'family' },
+    { taxId: 570, scientificName: 'Klebsiella', rank: 'genus' },
+    { taxId: 573, scientificName: 'Klebsiella pneumoniae', rank: 'species' },
+  ],
+  publicMetadataColumnNames: [],
+  collections: [],
+};
