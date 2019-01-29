c4ae7c060b1697c45e95a6bf9b9e2acd191e674c
Richard Goater <richard.goater27@gmail.com>
2019-01-29 18:21:42 +0000

    Make upload progress organism names more robust to length

--- src/upload/progress/Progress.react.js
+++ src/upload/progress/Progress.react.js
@@ -14,10 +14,15 @@ const Analysis = ({ data, showBreakdown }) => (
   <ul className="wgsa-upload-legend">
     {data.map(({ key, label, total, colour, ...analyses }) => (
       <li key={key}>
-        <i className="material-icons" style={{ color: colour }}>
-          stop
-        </i>
-        <strong>{label}</strong>: {total}
+        <span>
+          <i className="material-icons" style={{ color: colour }}>
+            stop
+          </i>
+          <strong className="wgsa-upload-legend-organism" title={label}>
+            {label}
+          </strong>
+          &nbsp;({total})
+        </span>
         {showBreakdown && (
           <ul>
             {Object.keys(analyses).map(analysisKey => {
