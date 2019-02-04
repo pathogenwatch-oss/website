7789a93f5e78068ddd8b91af79c92c182d92bc03
Richard Goater <richard.goater27@gmail.com>
2019-02-04 18:51:30 +0000

    Use chunk size of 64, quieter logging

--- src/upload/utils/hashWorker.js
+++ src/upload/utils/hashWorker.js
@@ -5,22 +5,23 @@ export function hash(file) {
     File.prototype.slice ||
     File.prototype.mozSlice ||
     File.prototype.webkitSlice;
-  const chunkSize = 1024 * 50;
+  const chunkSize = 1024 * 64;
   const chunks = Math.ceil(file.size / chunkSize);
   let currentChunk = 0;
   const fileReader = new FileReaderSync();
   const messageDigest = sha1.create().start();
 
+  console.log('hashing...');
   while (currentChunk < chunks) {
     const start = currentChunk * chunkSize;
     const end = start + chunkSize >= file.size ? file.size : start + chunkSize;
     const chunk = fileReader.readAsArrayBuffer(
       blobSlice.call(file, start, end)
     );
-    console.log(chunk);
     const binaryString = String.fromCharCode.apply(null, new Uint8Array(chunk));
     messageDigest.update(binaryString);
     currentChunk++;
   }
+  console.log('...digesting');
   return messageDigest.digest().toHex();
 }
