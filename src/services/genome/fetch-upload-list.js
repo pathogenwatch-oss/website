8f44360b0b9050fd5a4839d1d20984db6dad6565
Richard Goater <richard.goater27@gmail.com>
2019-03-06 20:17:06 +0000

    Return completed uploads in upload session list

--- src/services/genome/fetch-upload-list.js
+++ src/services/genome/fetch-upload-list.js
@@ -3,12 +3,19 @@ const Genome = require('models/genome');
 module.exports = function (props) {
   const $match = props.user ? { _user: props.user._id } : null;
   if (!$match) return [];
-  return (
-    Genome.aggregate([
-      { $match },
-      { $group: { _id: '$uploadedAt', total: { $sum: 1 } } },
-      { $project: { uploadedAt: '$_id', _id: 0, total: 1 } },
-      { $sort: { uploadedAt: -1 } },
-    ])
-  );
+  return Genome.aggregate([
+    { $match },
+    { $project: { uploadedAt: 1, upload: 1, fileId: 1 } },
+    {
+      $group: {
+        _id: '$uploadedAt',
+        total: { $sum: 1 },
+        complete: {
+          $sum: { $cond: [ { $or: [ '$upload.complete', { $ifNull: [ '$fileId', false ] } ] }, 1, 0 ] },
+        },
+      },
+    },
+    { $project: { uploadedAt: '$_id', _id: 0, total: 1, complete: 1 } },
+    { $sort: { uploadedAt: -1 } },
+  ]);
 };
