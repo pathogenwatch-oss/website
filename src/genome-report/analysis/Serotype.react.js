0be275df1643048d2a3d7006436a5804d2f0adff
Richard Goater <richard.goater27@gmail.com>
2019-03-18 20:01:16 +0000

    Use formatted serotype source

--- src/genome-report/analysis/Serotype.react.js
+++ src/genome-report/analysis/Serotype.react.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const sources = {
-  sistr: {
+  SISTR: {
     link: (
       <a
         href="http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0147101"
