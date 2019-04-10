2005ba351b8af2d2d9f6d66c093b634973a5813b
Corin Yeats <corin.yeats@gmail.com>
2019-04-10 17:35:52 +0100

    Test

--- src/genome-report/analysis/Kleborate.react.js
+++ src/genome-report/analysis/Kleborate.react.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classnames from 'classnames';
+import KleborateAMR from "./KleborateAMR.react";
 
 const kleborateTypingFields2 = [
   'Yersiniabactin',
@@ -24,49 +24,6 @@ const kleborateTypingFields1 = [
   'O_locus_confidence',
   'wzi',
 ];
-
-const kleborateAmrFields = [
-  'AGly',
-  'Col',
-  'Fcyn',
-  'Flq',
-  'Gly',
-  'MLS',
-  'Ntmdz',
-  'Phe',
-  'Rif',
-  'Sul',
-  'Tet',
-  'Tmt',
-  'Bla',
-  'Bla_Carb',
-  'Bla_ESBL',
-  'Bla_ESBL_inhR',
-  'Bla_broad',
-  'Bla_broad_inhR',
-];
-
-
-const klebAgentLinks = {
-  'AGly': {name: "Aminoglycosides", key: "AGS", type: "Aminoglycosides"},
-  'Col': {name: "Colistin", key: "CST", type: "Colistin"},
-  'Fcyn': {name: "Fosfomycin", key: "FOF", type: "Phosphonic Acid"},
-  'Flq': {name: "Fluoroquinolones", key: "FLQ", type: "Fluoroquinolones"},
-  'Gly': {name: "Glycopeptides", key: "GPA", type: "Glycopeptides"},
-  'MLS': {name: "Macrolides", key: "MAC", type: "Macrolides"},
-  'Ntmdz': {name: "Nitroimidazoles", key: "NIM", type: "Nitroimidazoles"},
-  'Phe': {name: "Phenicols", key: "PHE", type: "Phenicols"},
-  'Rif': {name: "Rifampicin", key: "RIF", type: "Rifamycin"},
-  'Sul': {name: "Sulfonamides", key: "SMX", type: "Sulfonamides"},
-  'Tet': {name: "Tetracycline", key: "TCY", type: "Tetracyclines"},
-  'Tmt': {name: "Trimethoprim", key: "TMP", type: "Diaminopyrimidine"},
-  'Bla': {name: "Beta-lactams", key: "BLA", type: "Beta-Lactams"},
-  'Bla_Carb': {name: "Carbapenems", key: "CBP", type: "Carbapenems"},
-  'Bla_ESBL': {name: "ESBLs", key: "EBL", type: "Extended Spectrum Beta-Lactams"},
-  'Bla_ESBL_inhR': {name: "ESBL Inhibitors", key: "EBI", type: "ESBL Inhibitors"},
-  'Bla_broad': {name: "Broad-Spectrum Cephalosporins", key: "CEP", type: "Broad-Spectrum Cephalosporins"},
-  'Bla_broad_inhR': {name: "BSBL Inhibitors", key: "BBI", type: "Broad-Spectrum Beta-Lactam Inhibitors"},
-};
 
 export default ({result}) => (
   <React.Fragment>
@@ -121,35 +78,7 @@ export default ({result}) => (
       </tr>
       </tbody>
     </table>
-    <table cellSpacing="0" className="wgsa-genome-report-amr wide bordered">
-      <caption>Resistance Profile</caption>
-      <thead>
-      <tr>
-        <th>Agent</th>
-        <th>Variants/Genes</th>
-      </tr>
-      </thead>
-      <tbody>
-      {kleborateAmrFields.map((klebName) => (
-        <tr
-          key={klebAgentLinks[klebName].name}
-          className={classnames({
-            'pw-genome-report-amr-present': result[klebName] !== '-',
-            'pw-genome-report-amr-resistant': result[klebName] !== '-',
-          })}
-        >
-          <td>{klebAgentLinks[klebName].name}</td>
-          <td className="pw-genome-report-amr-mechanisms">
-            {result[klebName]
-              .replace(/;/gi, ', ')
-              .replace(/\*/gi, '_homolog')
-              .replace(/\?/gi, '_fragment')
-            }
-          </td>
-        </tr>
-      ))}
-      </tbody>
-    </table>
+    <KleborateAMR result={result} />
   </React.Fragment>
 );
 
