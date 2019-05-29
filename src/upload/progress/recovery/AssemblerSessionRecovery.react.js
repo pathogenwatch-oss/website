8d537dcdc7b5d7d5a2c41555d1f1c99887d0a4a6
Richard Goater <richard.goater27@gmail.com>
2019-05-29 19:01:37 +0100

    WIP

--- src/upload/progress/recovery/AssemblerSessionRecovery.react.js
+++ src/upload/progress/recovery/AssemblerSessionRecovery.react.js
@@ -4,10 +4,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Recovery from './Recovery.react';
+import Spinner from '~/components/Spinner.react';
+import Fade from '~/components/fade';
 
 import { useAssemblerSession } from '../assembly/hooks';
 import { useAuthToken } from '~/auth/hooks';
 import { useAssemblerUsage } from '../../hooks';
+
+function useWait(time) {
+  const [ waited, setWaited ] = React.useState(false);
+  React.useEffect(() => {
+    setTimeout(() => setWaited(true), time);
+  }, []);
+  return waited;
+}
 
 const AssemblySessionRecovery = ({
   uploadedAt,
@@ -16,25 +26,34 @@ const AssemblySessionRecovery = ({
   useAuthToken();
   useAssemblerUsage(token);
   const [ session, error ] = useAssemblerSession(uploadedAt, token);
+  const waited = useWait(500);
 
-  if (error) {
-    return (
-      <section className="pw-upload-page">
+  let content;
+  if (waited && error) {
+    content = (
+      <section key="error" className="pw-upload-page">
         <h1>Sorry, there's a problem.</h1>
         {typeof error.message === 'string' ?
           (<p>{error.message}</p>) :
           error.message }
       </section>
     );
-  }
-
-  if (session) {
-    return (
-      <Recovery session={session} />
+  } else if (waited && session) {
+    content = <Recovery key="loaded" session={session} />;
+  } else {
+    content = (
+      <section key="loading" className="pw-upload-page pw-centre-text">
+        <Spinner />
+      </section>
     );
   }
 
-  return null;
+
+  return (
+    <Fade>
+      {content}
+    </Fade>
+  );
 };
 
 function mapStateToProps(state) {
