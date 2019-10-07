9a4bd91588114cc1eba58606bf9b233384d5ca6e
Corin Yeats <dr.yeats@gmail.com>
2019-10-07 09:31:52 +0000

    Inctyper Updates

--- src/genome-report/analysis/Inctyper.react.js
+++ src/genome-report/analysis/Inctyper.react.js
@@ -1,24 +1,67 @@
 import React from 'react';
+import InctyperContig from './InctyperContig.react';
 
 // Example record.
-// "Inc Matches": [
+// {
+//   "Library": "gram_positive",
+//   "Inc Matches": [
 //     {
-//     "Contig": "tig00000033|quiver",
-//     "Inc Match": "IncA/C_1__FJ705807",
-//     "Percent Identity": 98.321,
-//     "Match Coverage": 1.0,
-//     "Contig Start": 89119,
-//     "Contig End": 89535,
-//     "Reference Start": 1,
-//     "Reference End": 417
-//    }
-// ]
+//       "Contig": ".ERR419050.13",
+//       "Match ID": "rep19_3_CDS20(pSJH901)_CP000704",
+//       "Group": "RepA_N",
+//       "Inc Match": "rep19_3",
+//       "Percent Identity": 100.0,
+//       "Match Coverage": 100.0,
+//       "Contig Start": 20625,
+//       "Contig End": 21608,
+//       "Reference Start": 1,
+//       "Reference End": 984
+//     }
+//   ]
+// }
+
+function defaultView(message) {
+  return (
+    <React.Fragment>
+      <header className="pw-genome-report-section-header">
+        <h2>Plasmid Inc Types</h2>
+        <p>
+          <a
+            href="https://cge.cbs.dtu.dk/services/PlasmidFinder/"
+            target="_blank"
+            rel="noopener"
+          >Database sourced from https://cge.cbs.dtu.dk/services/PlasmidFinder/
+          </a>
+        </p>
+      </header>
+      <p>{message}</p>
+    </React.Fragment>
+  );
+}
 
 export default ({ analysis }) => {
   const { inctyper, paarsnp = { matches: [] } } = analysis;
-  const matches = inctyper['Inc Matches'];
 
-  matches.sort((a, b) => a.Contig.localeCompare(b.Contig));
+  if (!inctyper.hasOwnProperty('Inc Matches')) {
+    return defaultView('Inctyper was not run for this assembly.');
+  } else if (inctyper['Inc Matches'].length === 0) {
+    return defaultView('No matches reported.');
+  }
+
+  // Group inc matches by contig
+  const groupedMatches = inctyper['Inc Matches'].reduce((groups, match) => {
+    const updatedGroups = groups;
+    if (!updatedGroups.hasOwnProperty(match.Contig)) {
+      updatedGroups[match.Contig] = [];
+    }
+    updatedGroups[match.Contig].push(match);
+    return updatedGroups;
+  }, {});
+
+  // Create a set order of the contigs for alternative row styling
+  const contigOrder = Object.keys(groupedMatches).sort();
+
+  // matches.sort((a, b) => a.Contig.localeCompare(b.Contig));
 
   const amrMatches = {};
 
@@ -27,10 +70,8 @@ export default ({ analysis }) => {
     if (!match.query) {
       continue;
     }
-    if (
-      (paarsnp.hasOwnProperty('paar') && paarsnp.paar.includes(match.id)) ||
-      (paarsnp.hasOwnProperty('snp') && paarsnp.snp.includes(match.id))
-    ) {
+    if ((paarsnp.hasOwnProperty('paar') && paarsnp.paar.includes(match.id)) ||
+      (paarsnp.hasOwnProperty('snp') && paarsnp.snp.includes(match.id))) {
       if (!(match.query.id in amrMatches)) {
         amrMatches[match.query.id] = [];
       }
@@ -55,23 +96,23 @@ export default ({ analysis }) => {
       <table className="bordered wide" cellSpacing="0">
         <thead>
           <tr>
-            <th>Contig ID</th>
-            <th>Inc Type</th>
+            <th>Contig Inc Type</th>
+            {paarsnp.matches.length !== 0 && <th>Contig AMR Genes</th>}
+            <th>Match ID</th>
             <th>% Identity</th>
-            <th>Coverage</th>
-            <th>Linked AMR Genes</th>
+            <th>% Coverage</th>
           </tr>
         </thead>
         <tbody>
-          {matches.length ? (
-            matches.map(incMatch => (
-              <tr>
-                <td>{incMatch.Contig}</td>
-                <td>{incMatch['Inc Match']}</td>
-                <td>{incMatch['Percent Identity']}</td>
-                <td>{incMatch['Match Coverage']}</td>
-                <td>{(amrMatches[incMatch.Contig] || []).join(', ')}</td>
-              </tr>
+          {Object.keys(groupedMatches).length ? (
+            contigOrder.map((contigId, index) => (
+              <InctyperContig
+                amrMatches={amrMatches[contigId]}
+                library={inctyper.Library}
+                matches={groupedMatches[contigId]}
+                setBackground={index % 2 === 0}
+                displayAmr={paarsnp.matches.length !== 0}
+              />
             ))
           ) : (
             <tr>
@@ -85,3 +126,4 @@ export default ({ analysis }) => {
     </React.Fragment>
   );
 };
+
