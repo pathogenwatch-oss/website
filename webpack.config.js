5d41dcbc99bcf91e06038c32fdacf0dd17cb8fee
Richard Goater <richard.goater27@gmail.com>
2019-10-10 23:19:15 +0100

    More minor cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
-const WebpackMonitor = require('webpack-monitor');
-const NameAllModulesPlugin = require('name-all-modules-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
