dfb5ab344707d56f41855db530379c1036fc5237
Ben Taylor <ben.taylor@sanger.ac.uk>
2019-11-08 17:33:48 +0100

    Stream the output of dump

--- bin/dump-db-sample.js
+++ bin/dump-db-sample.js
@@ -9,6 +9,10 @@ const Genome = require('models/genome');
 const Collection = require('models/collection');
 const Analysis = require('models/analysis')
 
+function append(path, type, ...docs) {
+  docs.forEach(doc => fs.appendFileSync(path, JSON.stringify({ type, doc })))
+}
+
 async function main() {
   const { output } = argv.opts;
   if (!output || output == true ) {
@@ -16,41 +20,51 @@ async function main() {
     process.exit(1)
   }
 
+  fs.writeFileSync(output, '') // clear the document
   await mongoConnection.connect();
 
-  const documents = { genomes: [], analyses: [], collections: []}
+  const fileIds = new Set()
 
   const taxids = await Genome.distinct('analysis.speciator.speciesId', { public: true })
   for (let taxid of taxids) {
-    documents.genomes.push(await Genome.findOne({'analysis.speciator.speciesId': taxid, public: true }).lean())
+    const doc = await Genome.findOne({'analysis.speciator.speciesId': taxid, public: true }).lean()
+    append(output, 'genome', doc)
+    fileIds.add(doc.fileId)
   }
-  console.log(`Found ${documents.genomes.length} genomes`)
 
   const collection = await Collection.findOne({ 
     access: "public",
     organismId: "1280",
     token: "gn1lxjtquy8s-harris-et-al-2013" // https://pathogen.watch/collection/gn1lxjtquy8s-harris-et-al-2013
   }).lean()
+  append(output, 'collection', collection)
 
-  documents.collections.push(collection)
-  console.log(`Found ${documents.collections.length} collections`)
+  await Genome.find({ _id: { $in: collection.genomes }, public: true })
+    .lean()
+    .cursor()
+    .eachAsync(doc => {
+      fileIds.add(doc.fileId);
+      append(output, 'genome', doc);
+    })
 
-  const collectionGenomes = await Genome.find({ _id: { $in: collection.genomes }, public: true }).lean()
-  documents.genomes.push(...collectionGenomes)
-  console.log(`Found ${collectionGenomes.length} genomes`)
+  await Genome.find({'analysis.speciator.speciesId': "485", public: true })
+    .lean()
+    .cursor()
+    .eachAsync(doc => {
+      fileIds.add(doc.fileId);
+      append(output, 'genome', doc);
+    })
 
-  const gonoDocs = await Genome.find({'analysis.speciator.speciesId': "485", public: true }).lean()
-  documents.genomes.push(...gonoDocs);
-  console.log(`Found ${gonoDocs.length} genomes`)
+  await Analysis.find({ fileId: { $in: [...fileIds] }})
+    .lean()
+    .cursor()
+    .eachAsync(doc => {
+      fileIds.add(doc.fileId);
+      append(output, 'genome', doc);
+    })
 
-  const fileIds = new Set(documents.genomes.map(_ => _.fileId))
-  const analysisDocs = await Analysis.find({ fileId: { $in: [...fileIds] }}).lean()
-  documents.analyses = analysisDocs;
-  console.log(`Found ${documents.analyses.length} analyses`)
+  console.log("Done")
 
-  const content = JSON.stringify(documents)
-  fs.writeFileSync(output, content)
-  console.log(`Wrote output to ${output}`)
   return
 }
 
