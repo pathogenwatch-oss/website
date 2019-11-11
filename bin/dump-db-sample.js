419a246e45ae208eb2d8c346455ee31755733259
Ben Taylor <ben.taylor@sanger.ac.uk>
2019-11-11 14:42:18 +0100

    Record genomeIds for downloading the fastas

--- bin/dump-db-sample.js
+++ bin/dump-db-sample.js
@@ -35,28 +35,31 @@ async function main() {
   await mongoConnection.connect();
 
   const fileIds = new Set()
+  const genomeIds = new Set()
+
+  async function addGenome(doc) {
+    if (genomeIds.has(doc._id)) return;
+    append(output, 'genome', doc)
+    genomeIds.add(doc._id)
+    if (!doc.fileId || fileIds.has(doc.fileId)) return;
+    await addAnalysis(output, doc);
+    fileIds.add(doc.fileId)
+  }
 
   const taxids = await Genome.distinct('analysis.speciator.speciesId', { public: true })
   for (let taxid of taxids) {
-    const docs = await Genome.find({'analysis.speciator.speciesId': taxid, public: true }, { limit: 4 }).lean()
-    append(output, 'genome', ...docs)
-    for (let doc of docs) {
-      fileIds.add(doc.fileId)
-      await addAnalysis(output, doc)
-    }
+    await Genome.find({'analysis.speciator.speciesId': taxid, public: true }, { limit: 4 })
+      .lean()
+      .cursor()
+      .eachAsync(async doc => addGenome(doc))
   }
 
   await Genome.find({ reference: true })
     .lean()
     .cursor()
-    .eachAsync(async doc => {
-      if (fileIds.has(doc.fileId)) return;
-      fileIds.add(doc.fileId);
-      append(output, 'genome', doc);
-      await addAnalysis(output, doc)
-    })
+    .eachAsync(async doc => addGenome(doc))
 
-  const collection = await Collection.findOne({ 
+  const collection = await Collection.findOne({
     access: "public",
     organismId: "1280",
     token: "gn1lxjtquy8s-harris-et-al-2013" // https://pathogen.watch/collection/gn1lxjtquy8s-harris-et-al-2013
@@ -66,24 +69,12 @@ async function main() {
   await Genome.find({ _id: { $in: collection.genomes }, public: true })
     .lean()
     .cursor()
-    .eachAsync(async doc => {
-      if (fileIds.has(doc.fileId)) return;
-      fileIds.add(doc.fileId);
-      append(output, 'genome', doc);
-      await addAnalysis(output, doc)
-    })
+    .eachAsync(async doc => addGenome(doc))
 
-  await Genome.find({'analysis.speciator.speciesId': "485", public: true })
+  await Genome.find({'analysis.speciator.speciesId': "485", public: true }, { limit: 1000 })
     .lean()
     .cursor()
-    .eachAsync(async doc => {
-      if (fileIds.has(doc.fileId)) return;
-      fileIds.add(doc.fileId);
-      append(output, 'genome', doc);
-      await addAnalysis(output, doc)
-    })
-
-  append(output, '__fileIds', { fileIds })
+    .eachAsync(async doc => addGenome(doc))
 
   await Organism.find({})
     .lean()
@@ -92,6 +83,8 @@ async function main() {
       fileIds.add(doc.fileId);
       append(output, 'organisms', doc);
     })
+
+  append(output, '__ids', { fileIds: [...fileIds], genomeIds: [...genomeIds] })
 
   console.log("Done")
   await mongoConnection.close()
