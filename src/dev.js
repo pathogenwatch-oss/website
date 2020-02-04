fe857508ef89e2d7c2fd4a3608c127d6ad6e9877
Corin Yeats <corin.yeats@gmail.com>
2020-02-04 12:47:39 +0100

    Updated the front end

--- src/dev.js
+++ src/dev.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://penguin.linux.test:8080');
+  res.header('Access-Control-Allow-Origin', 'http://195.201.23.19:8080');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
