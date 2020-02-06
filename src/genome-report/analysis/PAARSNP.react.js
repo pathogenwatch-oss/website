be6467d3a9f04009c12235914f38c4b715a02ee0
Corin Yeats <dr.yeats@gmail.com>
2020-02-06 11:37:12 +0000

    Nicer genome report AMR names

--- src/genome-report/analysis/PAARSNP.react.js
+++ src/genome-report/analysis/PAARSNP.react.js
@@ -10,10 +10,56 @@ const libraryLink = {
 
 const originalPublicSet = new Set([ '1280', '1313', '485', '90370' ]);
 
+function formatElement(snp) {
+  const parts = snp.split('_');
+  if (parts[parts.length - 1].match(/^(-+|ins|[A-Za-z]+)(-?[0-9]+)(-+|del|[A-Za-z]+)$/)) {
+    return [ parts.slice(0, -1).join('_'), parts[parts.length - 1] ];
+  }
+
+  const result = [ '_disrupted', '_frameshift', '_truncated' ]
+    .find(variant => snp.endsWith(variant));
+  if (!!result) {
+    return [ snp.replace(result, ''), result ];
+  }
+
+  if (snp.match('aa_insert') || snp.match('nt_insert')) {
+    return [ parts.slice(0, -3).join('_'), parts.slice(-3).join('_') ];
+  }
+  return [ snp, '' ];
+}
+
+function extractLocation(snp) {
+  const location = snp.replace(/[A-Za-z_]+/g, '').split('-')[0];
+  return location !== '' ? parseInt(location, 10) : -1;
+}
+
+function formatElements(mechanisms, paar) {
+  const elementMap = mechanisms
+    .map(mechanism => (paar.includes(mechanism) ? [ mechanism, '' ] : formatElement(mechanism)))
+    .reduce((map, mechanismName) => {
+      if (!map[mechanismName[0]]) {
+        map[mechanismName[0]] = new Set();
+      }
+      if (mechanismName[1] !== '') {
+        map[mechanismName[0]].add(mechanismName[1]);
+      }
+      return map;
+    }, {});
+
+  return Object.keys(elementMap)
+    .map(name => name + ((!!elementMap[name] && elementMap[name].size !== 0) ?
+      `_${Array.from(elementMap[name])
+        .sort((a, b) => extractLocation(a) - extractLocation(b)).join('/')}` :
+      ''))
+    .sort()
+    .join('; ');
+}
+
 export default ({ result, genome }) => {
   const { organismId } = genome.analysis.speciator;
   const {
     antibiotics,
+    paar,
     library = {
       label: organismId,
       source: originalPublicSet.has(organismId) ? 'PUBLIC' : 'TESTING',
@@ -34,7 +80,7 @@ export default ({ result, genome }) => {
     ? antibiotics.filter(({ name }) => !hiddenColumns.has(name))
     : antibiotics;
 
-  const libraryUrl = `${libraryLink[library.source]}/${library.version}/${library.label}.toml`;
+  const libraryUrl = `${libraryLink[library.source]} /${library.version}/${library.label}.toml`;
 
   return (
     <React.Fragment>
@@ -67,7 +113,7 @@ export default ({ result, genome }) => {
                 {state.replace(/_/g, ' ').toLowerCase()}
               </td>
               <td className="pw-genome-report-amr-mechanisms">
-                {mechanisms.join(', ')}
+                {formatElements(mechanisms, paar)}
               </td>
             </tr>
           ))}
