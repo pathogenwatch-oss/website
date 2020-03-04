8cc2310bb8b6746b2f0cfd23b47ca63b0428b09a
Ben Taylor <ben.taylor@sanger.ac.uk>
2020-03-04 14:55:25 +0100

    Remove pathogenwatch-fasta-store

--- src/services/genome/file-path.js
+++
@@ -1,6 +0,0 @@
-const fastaStorage = require('pathogenwatch-fasta-store');
-const { fastaStoragePath } = require('configuration');
-
-module.exports = ({ fileId }) => Promise.resolve(
-  fastaStorage.getFilePath(fastaStoragePath, fileId)
-);
