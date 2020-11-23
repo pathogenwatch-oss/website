ac7c6eb9e4d1e1ebe0f9c4fef5eaf7fe19046311
Corin Yeats <corin.yeats@cgps.group>
2020-11-23 16:16:56 +0000

    Updated the CSV output for the new Kleborate format.

--- src/routes/download/utils/kleborate.js
+++ src/routes/download/utils/kleborate.js
@@ -2,16 +2,17 @@ module.exports.transformer = function (doc) {
   const record = {
     'Genome ID': doc._id.toString(),
     'Genome Name': doc.name,
-    'Version': doc.analysis.kleborate.__v
+    Version: doc.analysis.kleborate.__v,
+    'Kleborate version': doc.analysis.kleborate['Kleborate version'],
   };
 
   doc.analysis.kleborate.csv.forEach((item) => {
     if (item.set === '') {
-      record[item.name] = doc.analysis.kleborate[item.field]
+      record[item.name] = doc.analysis.kleborate[item.field];
     } else if (item.set === 'amr') {
-      record[item.name] = doc.analysis.kleborate[item.set][item.field].match
+      record[item.name] = doc.analysis.kleborate[item.set].classes[item.field];
     } else {
-      record[item.name] = doc.analysis.kleborate[item.set][item.field]
+      record[item.name] = doc.analysis.kleborate[item.set][item.field];
     }
   });
 
