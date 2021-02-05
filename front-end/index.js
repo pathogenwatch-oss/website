e5d4d2748aac9f45b5e4419345c19457b675ecda
Khalil <ka10@sanger.ac.uk>
2021-02-05 19:44:02 +0000

    Update index.js

--- front-end/index.js
+++ front-end/index.js
@@ -4,10 +4,10 @@ import { render } from 'react-dom';
 import config from './app/config.js';
 import App from './app';
 
-if (process.env.NODE_ENV !== 'production' && config.whyDidYouUpdate) {
-  const { whyDidYouUpdate } = require('why-did-you-update');
-  whyDidYouUpdate(React);
-}
+// if (process.env.NODE_ENV !== 'production' && config.whyDidYouUpdate) {
+//   const { whyDidYouUpdate } = require('why-did-you-update');
+//   whyDidYouUpdate(React);
+// }
 
 render(
   <App />,
