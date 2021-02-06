2f05e399cb338d848103a5c2c1c73b5ba06f6c5f
Khalil <ka10@sanger.ac.uk>
2021-02-06 16:32:07 +0000

    Fix eslint errors.

--- start.js
+++ start.js
@@ -1,13 +1,12 @@
 const express = require("express");
-
-const createServer = require('./server/index');
-const { BaseLogger } = require('./services/logger');
+const createServer = require("./server/index");
+const { BaseLogger } = require("./services/logger");
 
 Promise.resolve(new express())
   .then(createServer)
-  .then(() => BaseLogger.info('*** Application started ***'))
-  .catch(error => {
+  .then(() => BaseLogger.info("*** Application started ***"))
+  .catch((error) => {
     BaseLogger.error(error);
-    BaseLogger.error('*** Application not started ***');
+    BaseLogger.error("*** Application not started ***");
     return process.exit(1);
   });
