d496ac9cf3bd95a68b1d08a051019090b314d5bf
Corin Yeats <corin.yeats@cgps.group>
2021-02-10 11:27:22 +0000

    Removed internal search links from sarscov2-variants genome report.

--- front-end/genome-report/analysis/Sarscov2Variants.react.js
+++ front-end/genome-report/analysis/Sarscov2Variants.react.js
@@ -6,11 +6,6 @@ function generateDocLink(variant) {
   return (
     <div>
       <p>{variant}
-        <ExternalLink
-          to={`/genomes/all?genusId=694002&sarscov2-variants=${variant}&subspecies=SARS-CoV-2`}
-        >
-          {'PW Search'}
-        </ExternalLink>
         <ExternalLink to={`https://cgps.gitlab.io/cog-uk/type_variants/#${variant}`}>
           {'Source'}
         </ExternalLink>
