b69a28719a88de97c12b893b8bb76eb0acbcf74b
Ben Taylor <ben.taylor@sanger.ac.uk>
2021-06-10 14:59:15 +0200

    Don't debug creating mongo indexes

--- src/utils/mongoConnection.js
+++ src/utils/mongoConnection.js
@@ -38,7 +38,8 @@ function connect(callback) {
 mongoose.set('debug', (collection, ...args) => {
   if (collection === 'clustering' && args[0] === 'update') {
     LOGGER.debug([ collection, ...args.slice(0, 2), 'TOO_LONG', ...args.slice(3) ]);
-  } else LOGGER.debug([ collection, ...args ]);
+  } else if (args[0] === 'ensureIndex');
+  else LOGGER.debug([ collection, ...args ]);
 });
 
 module.exports.connect = connect;
