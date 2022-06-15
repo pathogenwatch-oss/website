8d2530923886efd66a9ab060acc99e29d2b8f9d7
corin.yeats <corin.yeats@cgps.group>
2022-06-15 17:48:07 +0100

    Added shigella query and fixed a bug in the amrwatch script.

--- bin/dump-records/dump-amrwatch.js
+++ bin/dump-records/dump-amrwatch.js
@@ -1,10 +1,11 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console,camelcase */
 const mongoConnection = require('utils/mongoConnection');
 const { ObjectId } = require('mongoose/lib/types');
 const Genome = require('models/genome');
 const argv = require('named-argv');
 const fs = require('fs');
 
+const separator = ",";
 const projection = {
   name: 1,
   latitude: 1,
@@ -22,7 +23,9 @@ const projection = {
   'analysis.kleborate.amr': 1,
   'analysis.kleborate.csv': 1,
   'analysis.serotype.value': 1,
+  'analysis.spn_pbp_amr': 1,
 };
+
 const _user = new ObjectId("623b3dac8f2efe62c2e69fa8");
 
 const queries = {
@@ -94,6 +97,13 @@ const queries = {
       'analysis.speciator.organismId': '28901',
     },
   },
+  shi: {
+    query: {
+      _user,
+      binned: false,
+      'analysis.speciator.genusId': '620',
+    },
+  },
   sty: {
     query: {
       _user,
@@ -109,16 +119,15 @@ const queries = {
     },
   },
 };
-
 function writeOutput(species, foundTasks, rows) {
   const header = [ "Name", "Latitude", "Longitude", "Country", "Year", "Month", "Day" ];
   const tasks = Object.keys(foundTasks);
   for (const task of tasks) {
-    header.push(foundTasks[task]);
+    header.push(foundTasks[task].join(separator));
   }
   const stream = fs.createWriteStream(`${species}.csv`);
-  stream.write(`${header.join(",")}\n`);
 
+  stream.write(`${header.join(separator)}\n`);
   for (const row of rows) {
     const values = [ row.name, row.latitude, row.longitude, row.country, row.year, row.month, row.day ];
     for (const task of tasks) {
@@ -126,7 +135,7 @@ function writeOutput(species, foundTasks, rows) {
         (!!row[task] && !!row[task][column]) ? values.push(row[task][column]) : '';
       }
     }
-    stream.write(`${values.join(",")}\n`);
+    stream.write(`${values.join(separator)}\n`);
   }
   stream.on('finish', () => {
     process.exit(0);
@@ -155,7 +164,8 @@ async function main() {
     day,
     month,
     year,
-    analysis: { mlst, mlst2, kleborate, serotype } = {},
+    // eslint-disable-next-line camelcase
+    analysis: { mlst, mlst2, kleborate, serotype, spn_pbp_amr } = {},
   } of genomes) {
     const metadata = { name, longitude, latitude, country, day, month, year };
     if (!!mlst) {
@@ -185,6 +195,12 @@ async function main() {
       metadata.serotype = { Serotype: serotype.value };
       if (!('serotype' in foundTasks)) {
         foundTasks.serotype = Object.keys(metadata.serotype);
+      }
+    }
+    if (!!spn_pbp_amr) {
+      metadata.spn_pbp_amr = spn_pbp_amr;
+      if (!('spn_pbp_amr' in foundTasks)) {
+        foundTasks.spn_pbp_amr = Object.keys(metadata.spn_pbp_amr);
       }
     }
     rows.push(metadata);
