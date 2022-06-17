04916190c2e796295090196c6fc57756432e8b51
corin.yeats <corin.yeats@cgps.group>
2022-06-17 08:53:17 +0100

    Added K. variicola and Kqpn organisms.

--- universal/organisms.js
+++ universal/organisms.js
@@ -6,6 +6,8 @@ module.exports = [
   { id: '1336', speciesId: '1336', genusId: '1301', nickname: 'sequi' },
   { id: '1646', speciesId: '1646', genusId: '1645', nickname: 'rensm' },
   { id: '573', speciesId: '573', genusId: '570', nickname: 'klepn' },
+  { id: '244366', speciesId: '244366', genusId: '570', nickname: 'kleva' },
+  { id: '1463165', speciesId: '1463165', genusId: '570', nickname: 'kleqp' },
   { id: '498019', speciesId: '498019', genusId: '36910', nickname: 'canar' },
   { id: '64320', speciesId: '64320', genusId: '11051', nickname: 'zikv', simple: true },
   { id: '90370', speciesId: '28901', genusId: '590', nickname: 'styphi' },
