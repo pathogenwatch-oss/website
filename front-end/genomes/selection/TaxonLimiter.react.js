06c6fad9e66a419dd748ffb48337a61adeb4a864
corin.yeats <corin.yeats@cgps.group>
2023-03-20 13:30:39 +0000

    Fixed default threshold.

--- front-end/genomes/selection/TaxonLimiter.react.js
+++ front-end/genomes/selection/TaxonLimiter.react.js
@@ -8,7 +8,7 @@ import Sorry from './Sorry.react';
 import { getCollectionSummaryOrganismId } from '~/genomes/create-collection-form/selectors';
 
 const TaxonLimiter = ({ type, organismId, amount, children }) => {
-  const limit = config[type][organismId] || config[type].default || 1000;
+  const limit = config[type][organismId] || config[type].default || 2000;
 
   if (amount > limit) {
     return (
