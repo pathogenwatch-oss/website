16b16452702cfd49afed0403ccf9e9ad45fabae6
Corin Yeats <corin.yeats@cgps.group>
2024-10-09 11:02:57 +0100

    Fixed Kaptive CSV download.

--- src/routes/download/utils/kaptive.js
+++ src/routes/download/utils/kaptive.js
@@ -6,12 +6,14 @@ module.exports.transformer = (doc) => {
     'Kaptive Version': doc.analysis.kaptive.kaptiveVersion,
   };
   for (const column of Object.keys(doc.analysis.kaptive.kLocus)) {
+    if (column === 'name') continue;
     // const kName = `K Locus: ${column}`;
-    record[column] = doc.analysis.kaptive.kLocus[column];
+    record[`${doc.analysis.kaptive.kLocus.name} locus: ${column}`] = doc.analysis.kaptive.kLocus[column];
   }
   for (const column of Object.keys(doc.analysis.kaptive.oLocus)) {
+    if (column === 'name') continue;
     // const oName = `O Locus: ${column}`;
-    record[column] = doc.analysis.kaptive.oLocus[column];
+    record[`${doc.analysis.kaptive.oLocus.name} locus: ${column}`] = doc.analysis.kaptive.oLocus[column];
   }
   return record;
 };
