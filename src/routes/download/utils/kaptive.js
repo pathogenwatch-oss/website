663b69ea340a1bbdafc41831f483915a3e8e2c47
CorinYeatsCGPS <107122521+CorinYeatsCGPS@users.noreply.github.com>
2024-10-16 12:27:10 +0100

    Kaptive fix for cases with no matches (#83)
    * Checks that Kaptive document exists before attempting to write CSV output.
    * Updated Kaptive version

--- src/routes/download/utils/kaptive.js
+++ src/routes/download/utils/kaptive.js
@@ -2,18 +2,21 @@ module.exports.transformer = (doc) => {
   const record = {
     'Genome ID': doc._id.toString(),
     'Genome Name': doc.name,
-    Version: doc.analysis.kaptive.__v,
-    'Kaptive Version': doc.analysis.kaptive.kaptiveVersion,
   };
-  for (const column of Object.keys(doc.analysis.kaptive.kLocus)) {
-    if (column === 'name') continue;
-    // const kName = `K Locus: ${column}`;
-    record[`${doc.analysis.kaptive.kLocus.name} locus: ${column}`] = doc.analysis.kaptive.kLocus[column];
-  }
-  for (const column of Object.keys(doc.analysis.kaptive.oLocus)) {
-    if (column === 'name') continue;
-    // const oName = `O Locus: ${column}`;
-    record[`${doc.analysis.kaptive.oLocus.name} locus: ${column}`] = doc.analysis.kaptive.oLocus[column];
+
+  if ('analysis' in doc && 'kaptive' in doc.analysis) {
+    record.Version = doc.analysis.kaptive.__v;
+    record['Kaptive Version'] = doc.analysis.kaptive.kaptiveVersion;
+    for (const column of Object.keys(doc.analysis.kaptive.kLocus)) {
+      if (column === 'name') continue;
+      // const kName = `K Locus: ${column}`;
+      record[`${doc.analysis.kaptive.kLocus.name} locus: ${column}`] = doc.analysis.kaptive.kLocus[column];
+    }
+    for (const column of Object.keys(doc.analysis.kaptive.oLocus)) {
+      if (column === 'name') continue;
+      // const oName = `O Locus: ${column}`;
+      record[`${doc.analysis.kaptive.oLocus.name} locus: ${column}`] = doc.analysis.kaptive.oLocus[column];
+    }
   }
   return record;
 };
