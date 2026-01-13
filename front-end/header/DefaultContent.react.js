05ff0c12277837f39a8a1d29d722a82771bfbcba
Corin Yeats <corin.yeats@cgps.group>
2026-01-13 15:55:23 +0000

    Added release banner.

--- front-end/header/DefaultContent.react.js
+++ front-end/header/DefaultContent.react.js
@@ -12,11 +12,41 @@ const OfflineContent = () => (
 
 export default ({ offline = isOffline() }) => {
   if (offline) {
-    return <OfflineContent />;
+    return <OfflineContent/>;
   }
 
   return (
     <nav className="wgsa-header-content mdl-navigation">
+      <span
+        style={ {
+          marginLeft: '24px',
+          marginRight: 'auto',
+          fontSize: '14px',
+          color: 'rgba(0, 0, 0, 0.54)',
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          minWidth: 0, // This allows the span to shrink below its content size
+          paddingRight: '16px',
+        } }
+        title="Major release scheduled for February 2026. See more at next.pathogen.watch."
+      >
+        <a
+          href="Release Plan" // Change to the real documentation link.
+          target="_blank"
+          rel="noopener"
+          style={ { color: '#673c90', textDecoration: 'underline' } }
+        >
+          Major release</a> scheduled for February 2026. See more at{ ' ' }
+        <a
+          href="https://next.pathogen.watch"
+          target="_blank"
+          rel="noopener"
+          style={ { color: '#673c90', textDecoration: 'underline' } }
+        >
+          next.pathogen.watch
+        </a>
+      </span>
       <NavLink to="/genomes">Genomes</NavLink>
       <NavLink to="/collections">Collections</NavLink>
       <NavLink to="/upload">Upload</NavLink>
@@ -43,11 +73,11 @@ export default ({ offline = isOffline() }) => {
         target="_blank"
         title="Follow @Pathogenwatch"
       >
-        <i className="cgps-twitter-icon" />
+        <i className="cgps-twitter-icon"/>
       </a>
       <a
         className="mdl-button mdl-button--icon"
-        href={`mailto:pathogenwatch@cgps.group?body=${encodeURIComponent('Please add a descriptive subject line and also edit the section below as appropriate.\n------\nTopic - Website / Data / Documentation / Other\nType - Feature Request / Error Report / Explanation Request\nAffected URL - the URL of the query page if relevant.\nBug Report - Browser / Operating system / Time & Date\n-----\n')}`}
+        href={ `mailto:pathogenwatch@cgps.group?body=${ encodeURIComponent('Please add a descriptive subject line and also edit the section below as appropriate.\n------\nTopic - Website / Data / Documentation / Other\nType - Feature Request / Error Report / Explanation Request\nAffected URL - the URL of the query page if relevant.\nBug Report - Browser / Operating system / Time & Date\n-----\n') }` }
         rel="noopener"
         target="_blank"
         title="Contact us"
